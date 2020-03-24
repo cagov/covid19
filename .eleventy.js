d1e4e2421d10515ee2c79ecc548e07dfb61a0ec7
carterm <carter.medlin@gmail.com>
2020-03-24 15:40:58 -0700

    Adding accordion page if used

--- .eleventy.js
+++ .eleventy.js
@@ -70,6 +70,8 @@ module.exports = function(eleventyConfig) {
       || page.date.toISOString()
   );
   
+  eleventyConfig.addPairedShortcode("dothisifcontentexists", (content, contentcontent, match) => 
+    contentcontent.match(match) ? content : "");
 
   eleventyConfig.htmlTemplateEngine = "njk";
 };
