3bd5b5d7befc3b360c917513594397a3f87b915a
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-03-30 15:30:16 -0700

    add inlined js build

---
+++ src/js/rollup.config.js
@@ -0,0 +1,12 @@
+import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
+module.exports = {
+  input: 'src/js/index.js',
+  output: {
+    file: 'pages/_includes/built.js',
+    format: 'esm'
+  },
+  plugins: [resolve(), terser()
+  ]
+};
