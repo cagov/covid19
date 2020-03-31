bde094c1a9ddb584d93726c6c92bb71cb96791ed
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-03-30 17:31:18 -0700

    oh ie11 when will you be gone

---
+++ src/js/es5.js
@@ -0,0 +1,5 @@
+import '@webcomponents/webcomponentsjs';
+import './polyfills/endswith.js';
+// import 'url-search-params-polyfill';
+import '@cagov/accordion';
+import '@cagov/step-list';
---
+++ src/js/polyfills/endswith.js
@@ -0,0 +1,8 @@
+if (!String.prototype.endsWith) {
+  String.prototype.endsWith = function (search, this_len) {
+    if (this_len === undefined || this_len > this.length) {
+      this_len = this.length;
+    }
+    return this.substring(this_len - search.length, this_len) === search;
+  };
+}
