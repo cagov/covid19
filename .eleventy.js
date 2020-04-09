e3654d2e492d8365a841642cd522a6f1db648c8c
carterm <carter.medlin@gmail.com>
2020-04-08 17:44:43 -0700

    reducing change

--- .eleventy.js
+++ .eleventy.js
@@ -91,7 +91,8 @@ module.exports = function(eleventyConfig) {
   // show or hide content based on page
   eleventyConfig.addPairedShortcode("pagesection", contentfrompage);
 
-  eleventyConfig.addFilter('contentfilter', html => html);
+  eleventyConfig.addFilter('contentfilter', code => code);
+       //.replace(/COVID-19/g,'COVID&#8288;-&#8288;19'));
 
   eleventyConfig.addFilter('jsonparse', json => JSON.parse(json));
 
