9a3e7c6e94e2d54138a4122c5d78cfd0000af9b6
carterm <carter.medlin@gmail.com>
2020-04-17 11:49:13 -0700

    Yay it works.  refine.

--- .eleventy.js
+++ .eleventy.js
@@ -153,6 +153,45 @@ module.exports = function(eleventyConfig) {
     }
     return html;
   });
+
+  eleventyConfig.addTransform("findaccordions2", function(html, outputPath) {
+    if(outputPath&&outputPath.endsWith(".html")) {
+      const dom = new JSDOM(html);
+      const document = dom.window.document;
+
+      for(const header of document.querySelectorAll('.carter-accordion')) {
+        const container = document.createElement('cwds-accordion');
+        const body = document.createElement('div');
+        body.className="card-body";
+        //body.setAttribute("aria-hidden","true");
+        //body.style.height="0px";
+
+        
+        header.parentNode.insertBefore(container, header);
+        container.appendChild(header);
+        container.appendChild(body);
+
+
+
+        let direct = document.querySelector('cwds-accordion + .carter-accordion-content');
+        while (direct) {
+          direct.parentNode.removeChild(direct);
+          body.appendChild(direct);
+          //direct.className="card-body";
+
+          direct = document.querySelector('cwds-accordion + .carter-accordion-content');
+        }
+
+        header.outerHTML=`<button class="card-header accordion-alpha" type="button" aria-expanded="false"><div class="accordion-title">${header.outerHTML}</div></button>`
+        body.outerHTML = `<div class="card-container" aria-hidden="true" style="height: 0px;">${body.outerHTML}</div>`;
+      }
+      return dom.serialize();
+    }
+    return html;
+  });
+
+
+
   eleventyConfig.addFilter('jsonparse', json => JSON.parse(json));
 
   const getLangRecord = tags => 
@@ -191,6 +230,6 @@ module.exports = function(eleventyConfig) {
       }
   });
 
-  eleventyConfig.htmlTemplateEngine = "njk,findaccordions";
+  eleventyConfig.htmlTemplateEngine = "njk,findaccordions,findaccordions2";
 };
 
