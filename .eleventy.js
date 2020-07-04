4bcf093276defb160c4e5c50507a2179557db6f3
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-07-04 09:27:03 -0700

    date disclaimer display

--- .eleventy.js
+++ .eleventy.js
@@ -152,7 +152,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addFilter('formatDateParts', function(datestring) {
     if(datestring) {
-      let output = new Date(datestring);
+      let output = new Date(`2020-${datestring}T00:00:00.000-07:00`);
       if(output) {
         return output.toLocaleDateString('en-US', { timeZone: 'America/Los_Angeles', day: 'numeric', month: 'long' })
       }  
@@ -162,7 +162,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addFilter('formatDatePartsPlus1', function(datestring) {
     if(datestring) {
-      let output = new Date(datestring);
+      let output = new Date(`2020-${datestring}T00:00:00.000-07:00`);
       if(output) {
         output.setDate(output.getDate() + 1)
         return output.toLocaleDateString('en-US', { timeZone: 'America/Los_Angeles', day: 'numeric', month: 'long' })
