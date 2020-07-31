3d3e6de607cc4fd3ad47f147c47e2696e34ab3f9
Jon Jensen <github@id.xjensen.com>
2020-07-31 10:19:40 -0700

    Extract ESM Rollup config

---
+++ src/js/rollup.config.all.js
@@ -0,0 +1,19 @@
+import alerts from './alerts/rollup.config';
+import es5 from './rollup.config.es5';
+import esm from './rollup.config';
+import plasma from './plasma/rollup.config';
+import survey from './survey/rollup.config';
+import telehealth from './telehealth/rollup.config';
+import whatwhere from './what-open-where/rollup.config';
+import video from './video/rollup.config';
+
+export default [
+  alerts,
+  es5,
+  esm,
+  plasma,
+  survey,
+  telehealth,
+  whatwhere,
+  video
+];
--- src/js/rollup.config.js
+++ src/js/rollup.config.js
@@ -1,27 +1,11 @@
 import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
-import alerts from './alerts/rollup.config';
-import es5 from './rollup.config.es5';
-import plasma from './plasma/rollup.config';
-import survey from './survey/rollup.config';
-import telehealth from './telehealth/rollup.config';
-import whatwhere from './what-open-where/rollup.config';
-import video from './video/rollup.config';
 
-export default [
-  {
-    input: 'src/js/index.js',
-    output: {
-      file: 'pages/_includes/built.js',
-      format: 'esm'
-    },
-    plugins: [resolve(), terser()]
+export default {
+  input: 'src/js/index.js',
+  output: {
+    file: 'pages/_includes/built.js',
+    format: 'esm'
   },
-  alerts,
-  es5,
-  plasma,
-  survey,
-  telehealth,
-  whatwhere,
-  video
-];
+  plugins: [resolve(), terser()]
+};
