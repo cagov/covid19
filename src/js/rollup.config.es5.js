fc197ef51e29293ed77b62f581d8247dd4abc3de
Jon Jensen <github@id.xjensen.com>
2020-07-31 10:08:18 -0700

    Extract ES5 Rollup config

---
+++ src/js/rollup.config.es5.js
@@ -0,0 +1,37 @@
+import path from 'path';
+import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+  input: 'src/js/es5.js',
+  output: {
+    file: 'docs/js/es5.js',
+    format: 'umd'
+  },
+  moduleContext: {
+    // https://rollupjs.org/guide/en/#error-this-is-undefined
+    [path.resolve('./node_modules/whatwg-fetch/fetch.js')]: 'window'
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            modules: false,
+            targets: {
+              browsers: '> 1%'
+            }
+          }
+        ]
+      ]
+    }),
+    terser()
+  ]
+};
--- src/js/rollup.config.js
+++ src/js/rollup.config.js
@@ -1,9 +1,7 @@
-import path from 'path';
 import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
-import babel from '@rollup/plugin-babel';
-import commonjs from '@rollup/plugin-commonjs';
 import alerts from './alerts/rollup.config';
+import es5 from './rollup.config.es5';
 import plasma from './plasma/rollup.config';
 import survey from './survey/rollup.config';
 import telehealth from './telehealth/rollup.config';
@@ -19,38 +17,8 @@ export default [
     },
     plugins: [resolve(), terser()]
   },
-  {
-    input: 'src/js/es5.js',
-    output: {
-      file: 'docs/js/es5.js',
-      format: 'umd'
-    },
-    moduleContext: {
-      // https://rollupjs.org/guide/en/#error-this-is-undefined
-      [path.resolve('./node_modules/whatwg-fetch/fetch.js')]: 'window'
-    },
-    plugins: [
-      resolve(),
-      commonjs(),
-      babel({
-        babelHelpers: 'bundled',
-        exclude: 'node_modules/**',
-        presets: [
-          [
-            '@babel/preset-env',
-            {
-              modules: false,
-              targets: {
-                browsers: '> 1%'
-              }
-            }
-          ]
-        ]
-      }),
-      terser()
-    ]
-  },
   alerts,
+  es5,
   plasma,
   survey,
   telehealth,
