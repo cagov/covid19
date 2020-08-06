c381fb632961e2804466d346d14e503aaf36c63b
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-08-06 04:45:44 -0700

    no

--- src/js/search/index.js
+++ src/js/search/index.js
@@ -1,46 +1,19 @@
-/**
- * Animates the placeholder attribute so text appars as if it is being typed for you.
- */
-// site.animateSearchPlacholder = function(){
-//   var string   = window.innerWidth < 1024 ? "SEARCH" : "What are you looking for?";
-//   var interval = 50;
-//   var letters  = string.split("");
-//   var index    = 0;
-
-//   $("#header-search-site").attr("placeholder", "");
-
-//   site.placeholderInterval = window.setInterval(function(){
-//     current = $("#header-search-site").attr("placeholder");
-
-//     if(letters[index]){
-//       $("#header-search-site").attr("placeholder", current + letters[index]);
-//       index += 1;
-//     } else {
-//       clearInterval(site.placeholderInterval);
-//     }
-
-//   }, interval);
-// };
-
-
-// // Add a class when the search fields are focused
-// $("body").on("focus", "#header-search-site, .header-search-button, .header-search-label", function(){
-//   $(".header-search").addClass("focused");
-// });
-// $("body").on("focus", ".expanded-menu-search-label, .expanded-menu-search-field, .expanded-menu-search-button", function(){
-//   $(".expanded-menu-search").addClass("focused");
-// });
-
-// // Remove a class when the search fields are blurred
-// $("body").on("blur", "#header-search-site, .header-search-button, .header-search-label", function(){
-//   $(".header-search").removeClass("focused");
-// });
-// $("body").on("blur", "expanded-menu-search-label, .expanded-menu-search-field, .expanded-menu-search-button", function(){
-//   $(".expanded-menu-search").removeClass("focused");
-// });  
-
-/**
-   * DO THIS ON PAGE LOAD
-   */
-  // site.animateSearchPlacholder();
-
+// Add a class when the search fields are focused, remove on blur, taken from trilogy and dejquerified
+document.querySelector('.header-search-field').addEventListener('focus', () => {
+   document.querySelector('.header-search').classList.add('focused');
+})
+document.querySelector('.header-search-button').addEventListener('focus', () => {
+   document.querySelector('.header-search').classList.add('focused');
+})
+document.querySelector('.header-search-label').addEventListener('focus', () => {
+   document.querySelector('.header-search').classList.add('focused');
+})
+document.querySelector('.header-search-field').addEventListener('blur', () => {
+   document.querySelector('.header-search').classList.remove('focused');
+})
+document.querySelector('.header-search-button').addEventListener('blur', () => {
+   document.querySelector('.header-search').classList.remove('focused');
+})
+document.querySelector('.header-search-label').addEventListener('blur', () => {
+   document.querySelector('.header-search').classList.remove('focused');
+})
