aca2e1adb1afc7e18c9a25f4db95ec95423372a7
Jon Jensen <github@id.xjensen.com>
2020-08-15 21:57:22 -0700

    Add new Rollup files

---
+++ src/js/rollup.config.es5.js
@@ -0,0 +1,41 @@
+import path from 'path';
+import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+
+export default {
+  input: 'src/js/es5.js',
+  output: {
+    file: 'docs/js/es5.js',
+    format: 'cjs'
+  },
+  moduleContext: {
+    // whatwg-fetch angers Rollup due to ancient use of 'this'.
+    // This fix is not essential to function. It removes a warning during the build process.
+    // https://rollupjs.org/guide/en/#error-this-is-undefined
+    [path.resolve('./node_modules/whatwg-fetch/fetch.js')]: 'window'
+  },
+  plugins: [
+    resolve({
+      browser: true
+    }),
+    commonjs(),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: ['node_modules/@babel/**'],
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            modules: false,
+            targets: {
+              browsers: '> 1%'
+            }
+          }
+        ]
+      ]
+    }),
+    terser()
+  ]
+};
