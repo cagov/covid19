5bd315af366a6781bfc9bb176f238265ce24bf21
Jon Jensen <github@id.xjensen.com>
2020-08-17 13:36:33 -0700

    Check for IntersectionObserver compatibility

--- src/js/menu/index.js
+++ src/js/menu/index.js
@@ -54,19 +54,21 @@ class CAGOVOverlayNav extends window.HTMLElement {
 }
 window.customElements.define('cagov-navoverlay', CAGOVOverlayNav);
 
-// Adding shadow on scroll
+// Add shadow on scroll.
+// Don't bother in older browsers that don't support IntersectionObserver. It's just a shadow!
+if (('IntersectionObserver' in window) && ('isIntersecting' in window.IntersectionObserverEntry.prototype)) {
+  const mainNav = document.querySelector('.header');
+  const scrollSpy = document.querySelector('.nav-scroll-spy');
 
-const mainNav = document.querySelector('.header');
-const scrollSpy = document.querySelector('.nav-scroll-spy');
+  // Remove the box-shadow if the scrollSpy element is "visible". Otherwise add it.
+  const observer = new window.IntersectionObserver((entries) => {
+    // entries[0] will just be the passed-in scrollSpy element.
+    if (entries[0].isIntersecting) {
+      mainNav.classList.remove('box-shadow');
+    } else {
+      mainNav.classList.add('box-shadow');
+    }
+  });
 
-// Remove the box-shadow if the scrollSpy element is "visible". Otherwise add it.
-const observer = new window.IntersectionObserver((entries) => {
-  // entries[0] will just be the passed-in scrollSpy element.
-  if (entries[0].isIntersecting) {
-    mainNav.classList.remove('box-shadow');
-  } else {
-    mainNav.classList.add('box-shadow');
-  }
-});
-
-observer.observe(scrollSpy);
+  observer.observe(scrollSpy);
+}
