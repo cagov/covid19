22e9d1331e297b1e6427cefcc2069fef3374a5f6
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-08-18 14:23:06 -0700

    chuck twitter

--- src/js/twitter/index.js
+++
@@ -1,9 +0,0 @@
-export default function loadTwitterFeed() {
-  if(window.innerWidth >= 1024 || window.innerHeight >= 1024) {
-    let el = document.querySelector('.twitter-feed')
-    el.innerHTML = `<a class="twitter-timeline" data-height="400" data-width="100%" href="https://twitter.com/CAPublicHealth?ref_src=twsrc%5Etfw">Tweets by CAPublicHealth</a>`;
-    var newScript = document.createElement("script");
-    newScript.src = "https://platform.twitter.com/widgets.js";
-    document.head.appendChild(newScript);
-  }
-}
