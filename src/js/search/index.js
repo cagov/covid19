440772083f6c3dee8f59b1bceeb4eff6cdf3fa59
konstantin <61205810+koryfey@users.noreply.github.com>
2020-08-24 16:17:10 -0700

    Updated header items for mobile

--- src/js/search/index.js
+++ src/js/search/index.js
@@ -16,4 +16,26 @@ document.querySelector('.header-search-button').addEventListener('blur', () => {
 })
 document.querySelector('.header-search-label').addEventListener('blur', () => {
    document.querySelector('.header-search').classList.remove('focused');
-})
+});
+
+
+
+
+
+// media query event handler
+if (matchMedia) {
+   const mq = window.matchMedia("(min-width: 768px)");
+   mq.addListener(WidthChange);
+   WidthChange(mq);
+   }
+   
+   // media query change
+   function WidthChange(mq) {
+   if (mq.matches) {
+     document.getElementById("header-search-site").placeholder = "Search this site";
+     document.querySelector('#dropdown-text').innerText = 'Select language';
+   } else {
+      document.querySelector('#dropdown-text').innerText = 'Language';
+      document.getElementById("header-search-site").placeholder = "SEARCH";
+   }
+   }
