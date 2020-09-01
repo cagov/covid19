f419fa976eefd8aaadb002ae9c17e1571a0815ac
Jon Jensen <github@id.xjensen.com>
2020-08-31 23:08:48 -0700

    Edits to survey component template

--- src/js/survey/template.js
+++ src/js/survey/template.js
@@ -8,13 +8,11 @@ export default function (incomingUrl, incomingPrompt) {
     surveyPrompt = incomingPrompt;
   }
   return `<div role="alert">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12 alert-left js-disclaimer">
-          <p class="lead bold">${surveyPrompt}</p>
-          <button class="btn btn-secondary js-dismiss-survey" type="button" aria-label="Close" data-dismiss="alert"><span aria-hidden="true">Dismiss</span></button>
-          <a href="${surveyUrl}" target="_new" class="js-goto-survey"><button class="btn btn-primary" type="button" aria-label="Open survey" data-dismiss="alert"><span aria-hidden="true">OK</span></button></a>
-        </div>
+    <div class="survey">
+      <div class="survey-content">
+        <p class="survey-content-prompt">${surveyPrompt}</p>
+        <button class="btn btn-secondary survey-content-dismiss js-dismiss-survey" type="button" aria-label="Close" data-dismiss="alert"><span aria-hidden="true">Dismiss</span></button>
+        <a href="${surveyUrl}" target="_new" class="survey-content-go js-goto-survey"><button class="btn btn-primary" type="button" aria-label="Open survey" data-dismiss="alert"><span aria-hidden="true">OK</span></button></a>
       </div>
     </div>
   </div>`;
