bb03c7457a09c9e7a46ce631656c7b970ea146d5
Jon Jensen <github@id.xjensen.com>
2020-09-01 16:56:20 -0700

    Pill-shaped survey buttons

--- src/js/survey/template.js
+++ src/js/survey/template.js
@@ -11,8 +11,8 @@ export default function (incomingUrl, incomingPrompt) {
     <div class="survey">
       <div class="survey-content">
         <p class="survey-content-prompt">${surveyPrompt}</p>
-        <button class="btn btn-secondary survey-content-dismiss js-dismiss-survey" type="button" aria-label="Close" data-dismiss="alert"><span aria-hidden="true">Dismiss</span></button>
-        <a href="${surveyUrl}" target="_new" class="survey-content-go js-goto-survey"><button class="btn btn-primary" type="button" aria-label="Open survey" data-dismiss="alert"><span aria-hidden="true">OK</span></button></a>
+        <button class="action-link pt-2 pb-2 px-4 survey-content-dismiss js-dismiss-survey" type="button" aria-label="Close" data-dismiss="alert"><span aria-hidden="true">Dismiss</span></button>
+        <a href="${surveyUrl}" target="_new" class="action-link pt-2 pb-2 px-4 survey-content-go js-goto-survey"><button type="button" aria-label="Open survey" data-dismiss="alert"><span aria-hidden="true">OK</span></button></a>
       </div>
     </div>
   </div>`;
