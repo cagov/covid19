27840185e102571454cc68a632a1ec4f74d1ef2d
Jon Jensen <github@id.xjensen.com>
2020-09-03 15:42:05 -0700

    Remove what-open-where

--- src/js/rollup.config.all.js
+++ src/js/rollup.config.all.js
@@ -4,7 +4,6 @@ import esm from './rollup.config';
 import plasma from './plasma/rollup.config';
 import reopening from './roadmap/rollup.config';
 import telehealth from './telehealth/rollup.config';
-import whatwhere from './what-open-where/rollup.config';
 import video from './video/rollup.config';
 
 // Combines all the Rollup files into one.
@@ -14,7 +13,6 @@ export default [
   plasma,
   reopening,
   telehealth,
-  whatwhere,
   video,
   // Only include these rollups in development mode.
   ...((process.env.NODE_ENV === 'development') ? [] : [es5])
