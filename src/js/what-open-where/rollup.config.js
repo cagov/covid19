db99710dc43f5adaa36c9441acb3ec7b0946da85
Jon Jensen <github@id.xjensen.com>
2020-09-03 14:26:56 -0700

    Add dev output paths to rollups

--- src/js/what-open-where/rollup.config.js
+++ src/js/what-open-where/rollup.config.js
@@ -2,10 +2,14 @@ import resolve from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json';
 import { terser } from 'rollup-plugin-terser';
 
+const devOutputPath = 'docs/js/what-open-where.js';
+const prodOutputPath = 'pages/_buildoutput/what-open-where.js';
+const outputPath = (process.env.NODE_ENV === 'development') ? devOutputPath : prodOutputPath;
+
 export default {
   input: 'src/js/what-open-where/index.js',
   output: {
-    file: 'pages/_buildoutput/what-open-where.js',
+    file: outputPath,
     format: 'esm'
   },
   plugins: [resolve(), json(), terser()]
