d246a3a6c03bd7f2a76447bbe558fe6791e10510
carterm <carter.medlin@gmail.com>
2020-09-08 14:47:31 -0700

    Big improvements to the way json data is validated

--- pages/_data/pubData.js
+++ pages/_data/pubData.js
@@ -1,5 +1,24 @@
 const languages = require('./langData.json').languages;
-const camelCase = (string) => string.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
+const camelCase = string => string.replace(/-([a-z])/g, g => g[1].toUpperCase());
+
+// Reusable function for checking for the existing keys
+const validateColumnExists = (dataset,tablename,...colnames) => {
+  const table = dataset[tablename];
+  if(!table) return `Table ${tablename} is missing.`;
+  for(const colname of colnames)
+    if(table.some(x=>!x[colname])) return `${tablename} is missing at least one '${colname}.`;
+  return null;
+}
+
+const validatePeopleWantToKnow = dataset => 
+  validateColumnExists(dataset,'Table1','text','_url') 
+  || validateColumnExists(dataset,'Table2','text','_url') 
+;
+
+const validateReopeningMatrixData = dataset => 
+  validateColumnExists(dataset,'Table1','colorLabel','_Color label','New cases','Positive tests','description','County tier')
+  || validateColumnExists(dataset,'Table3','_id','text')
+;
 
 // Pages with translations.
 // The 'slug' is the filename prefix.
@@ -10,8 +29,8 @@ const files = [
   { slug: 'do-dont', split: false },
   { slug: 'footer-data', split: false },
   { slug: 'homepage-text', split: true },
-  { slug: 'people-want-to-know', split: true },
-  { slug: 'reopening-matrix-data', split: true },
+  { slug: 'people-want-to-know', split: true, tableValidator: validatePeopleWantToKnow },
+  { slug: 'reopening-matrix-data', split: true, tableValidator: validateReopeningMatrixData },
   { slug: 'reopening-roadmap-activity-data', split: true },
   { slug: 'was-this-page-helpful', split: true }
 ];
@@ -34,7 +53,15 @@ const data = languages.reduce((katamari, language) => {
   katamari[language.id] = files.reduce((tumbleweed, file) => {
     const nonEnglishDir = (file.split) ? 'translated-posts' : 'wordpress-posts';
     const parentDir = (language.id === 'en') ? 'wordpress-posts' : nonEnglishDir;
-    tumbleweed[camelCase(file.slug)] = require(`../${parentDir}/${file.slug}${language.filepostfix}.json`);
+    const path = `../${parentDir}/${file.slug}${language.filepostfix}.json`;
+    const tableData = require(path);
+    if(file.tableValidator) {
+      var errorMessage = file.tableValidator(tableData);
+      if(errorMessage) {
+        throw new Error(`${path}\n${errorMessage}`);
+      }
+    }
+    tumbleweed[camelCase(file.slug)] = tableData;
     return tumbleweed;
   }, {});
   return katamari;
