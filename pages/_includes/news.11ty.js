9ca92007395f73d138322b7bbd3b8dc470ce666c
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-09-09 08:37:31 -0700

    put date format back in

--- pages/_includes/news.11ty.js
+++ pages/_includes/news.11ty.js
@@ -2,22 +2,45 @@ class News {
   data() {
     return {
       layout: "page.njk",
-      publishdate: "today"
+      publishdate: "today",
     };
   }
 
-  render(data) { 
-    return `${data.collections.covidGuidance.map(item => {
-      return `<div class="card border-0 border-bottom rounded-0" lang="en-US">
+  
+  render(data) {
+    const locales = "en-US";
+    const timeZone = "America/Los_Angeles";
+    return `${data.collections.covidGuidance
+      .map((item) => {
+        let pressdate = new Date(item.data.publishdate);
+        return `<div class="card border-0 border-bottom rounded-0" lang="en-US">
         <div class="card-body pl-0" lang="en-US">
           <h2 class="card-title lead" lang="en-US">
-            <a href="${ item.data.url }" lang="en-US" hreflang="en-US">${ item.data.title }</a>
+            <a href="${item.data.url}" lang="en-US" hreflang="en-US">${
+          item.data.title
+        }</a>
           </h2>
-          <p class="card-text" lang="en-US"><small class="text-muted"><!--${ item.fileSlug } -->${ item.data.publishdate } </small></p>
-          <div class="card-text" lang="en-US">${ (item.data.meta.length > 221) ? item.data.meta.slice(0,220)+'...' : item.data.meta }</div>
+          <p class="card-text" lang="en-US"><small class="text-muted"><!--${
+            item.fileSlug
+          } -->${pressdate.toLocaleDateString(locales, {
+          timeZone,
+          day: "numeric",
+          month: "long",
+          year: "numeric",
+        })} at ${pressdate.toLocaleTimeString(locales, {
+          timeZone,
+          hour: "numeric",
+          minute: "numeric",
+        })} </small></p>
+          <div class="card-text" lang="en-US">${
+            item.data.meta.length > 221
+              ? item.data.meta.slice(0, 220) + "..."
+              : item.data.meta
+          }</div>
         </div>
       </div>`;
-      }).join(' ')}`
+      })
+      .join(" ")}`;
   }
 }
 
