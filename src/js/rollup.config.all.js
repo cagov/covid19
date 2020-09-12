f18134638dbc31a1daddfd1263084ed07b051f3e
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-09-12 09:03:10 -0700

    only build most of the js bundles when the files in those dirs changed after the last generated file

--- src/js/rollup.config.all.js
+++ src/js/rollup.config.all.js
@@ -6,14 +6,32 @@ import reopening from './roadmap/rollup.config';
 import telehealth from './telehealth/rollup.config';
 import video from './video/rollup.config';
 
+import fs from 'fs';
+import path from 'path';
+const getFileUpdatedDate = (path) => {
+  const stats = fs.statSync(path)
+  return stats.mtime
+}
+const getLastUpdatedFile = (directoryPath) => {
+  let files = fs.readdirSync(directoryPath);
+  let latestFileTime = new Date('01/01/2020');
+  files.forEach(function (file) {
+    let thisFileUpdate = getFileUpdatedDate(directoryPath + file);
+    if(thisFileUpdate > latestFileTime) {
+      latestFileTime = thisFileUpdate;
+    }
+  });
+  return latestFileTime;
+}
+
 // Combines all the Rollup files into one.
 export default [
-  alerts,
+  ...((getLastUpdatedFile(__dirname + '/alerts/') > getFileUpdatedDate(path.join(__dirname, '../../docs/js/alerts.js'))) ? [alerts] : []),
+  ...((getLastUpdatedFile(__dirname + '/plasma/') > getFileUpdatedDate(path.join(__dirname, '../../docs/js/plasma.js'))) ? [plasma] : []),
+  ...((getLastUpdatedFile(__dirname + '/roadmap/') > getFileUpdatedDate(path.join(__dirname, '../../docs/js/roadmap.js'))) ? [reopening] : []),
+  ...((getLastUpdatedFile(__dirname + '/telehealth/') > getFileUpdatedDate(path.join(__dirname, '../../docs/js/telehealth.js'))) ? [telehealth] : []),
+  ...((getLastUpdatedFile(__dirname + '/video/') > getFileUpdatedDate(path.join(__dirname, '../../docs/js/video.js'))) ? [video] : []),
   esm,
-  plasma,
-  reopening,
-  telehealth,
-  video,
   // Don't include ES5 file in dev mode.
   ...((process.env.NODE_ENV === 'development') ? [] : [es5])
 ];
