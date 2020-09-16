6077a934ab82874e9d6cde90b544cf6f1282a1ec
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-09-16 13:29:39 -0700

    bottom toggle works

--- src/js/dashboard/index.js
+++ src/js/dashboard/index.js
@@ -1,51 +1,26 @@
-let ethnicityEmbed = /*html*/`<object class='tableauViz' width='100%' height='327px' style='display:none;'>
-<param name='host_url' value='https%3A%2F%2Ftableau.cdt.ca.gov%2F' /> 
-<param name='name' value='COVID-19Webversion2020-09-14Alameda&#47;Ethnicity' />
-<param name='embed_code_version' value='3' /> 
-<param name='site_root' value='' />
-<param name='tabs' value='no' />
-<param name='toolbar' value='yes' />
-<param name='showAppBanner' value='false' />
-</object>`;
-let genderEmbed = /*html*/`<object class='tableauViz' width='100%' height='327px' style='display:none;'>
-<param name='host_url' value='https%3A%2F%2Ftableau.cdt.ca.gov%2F' />  
-<param name='name' value='COVID-19Webversion2020-09-14Alameda&#47;Gender' />
-<param name='embed_code_version' value='3' /> 
-<param name='site_root' value='' />
-<param name='tabs' value='no' />
-<param name='toolbar' value='yes' />
-<param name='showAppBanner' value='false' />
-</object>`;
-let ageEmbed = /*html*/`<object class='tableauViz' width='100%' height='327px' style='display:none;'>
-<param name='host_url' value='https%3A%2F%2Ftableau.cdt.ca.gov%2F' />  
-<param name='name' value='COVID-19Webversion2020-09-14Alameda&#47;Age' />
-<param name='embed_code_version' value='3' /> 
-<param name='site_root' value='' />
-<param name='tabs' value='no' />
-<param name='toolbar' value='yes' />
-<param name='showAppBanner' value='false' />
-</object>`;
-
 function resetToggles() {
   togglers.forEach(toggle => {
     toggle.classList.remove('toggle-active')  
   });
+  document.getElementById('gender-graph').style.display = 'none';
+  document.getElementById('ethnicity-graph').style.display = 'none';
+  document.getElementById('age-graph').style.display = 'none';
 }
 
-let toggleTarget = document.querySelector('.js-toggle-group-target')
-let togglers = document.querySelectorAll('.js-toggle-group')
+let togglers = document.querySelectorAll('.js-toggle-group');
+document.getElementById('ethnicity-graph').style.display = 'block';
 togglers.forEach(toggle => {
   toggle.addEventListener('click',function(event) {
     event.preventDefault();
     resetToggles();
     if(this.classList.contains('gender')) {
-      toggleTarget.innerHTML = genderEmbed;
+      document.getElementById('gender-graph').style.display = 'block';
     }
     if(this.classList.contains('age')) {
-      toggleTarget.innerHTML = ageEmbed;
+      document.getElementById('age-graph').style.display = 'block';
     }
     if(this.classList.contains('ethnicity')) {
-      toggleTarget.innerHTML = ethnicityEmbed;
+      document.getElementById('ethnicity-graph').style.display = 'block';
     }
     this.classList.add('toggle-active');
   })
