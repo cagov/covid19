edb8b218c6cd47d40e8a7a85e46354eef2a5f6bc
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-09-18 11:53:06 -0700

    use new embed code style that allows filter switching

--- src/js/dashboard/index.js
+++ src/js/dashboard/index.js
@@ -1,3 +1,39 @@
+function displayChart(containerSelector,width,height,url) {
+  let chartContainer = document.querySelector(containerSelector);
+  let chartActiveSheet = null;
+  var chartURL = url;
+  let chartOptions = {
+    width: width+'px',
+    height: height+'px'
+  }
+  return new tableau.Viz(chartContainer, chartURL, chartOptions);
+}
+/*
+onFirstInteractive: function () {
+  casesChartActiveSheet = viz.getWorkbook().getActiveSheet().getWorksheets()[1];
+}
+This is a working way to switch counties: activeSheet.applyFilterAsync("County", "Yolo", tableau.FilterUpdateType.REPLACE);
+*/
+
+// these are county toggles and state toggles
+let casesChart = displayChart('#casesChart',1000,547,"https://tableau.cdt.ca.gov/views/StateDashboard-CleanSources/1_1State-Reported?:origin=card_share_link&:embed=n");
+// "https://tableau.cdt.ca.gov/views/Filter/3_1County-Reported?:showAppBanner=false&:display_count=n&:showVizHome=n&:origin=viz_share_link";
+
+let testingChart = displayChart('#testingChart',1000,620,"https://tableau.cdt.ca.gov/views/StateDashboard-CleanSources/5_1StateTesting?:origin=card_share_link&:embed=n")
+// https://tableau.cdt.ca.gov/views/StateDashboard-CleanSources/6_1CountyTesting?:origin=card_share_link&:embed=n
+
+let hospitalChart = displayChart('#hospitalChart',1000,520,"https://tableau.cdt.ca.gov/views/StateDashboard-CleanSources/7_1StateHosp?:origin=card_share_link&:embed=n")
+// https://tableau.cdt.ca.gov/views/StateDashboard-CleanSources/9_1CountyHosp?:origin=card_share_link&:embed=n
+
+
+// this chart does not toggle
+let mapChart = displayChart('#mapChartContainer', 700,747, 'https://tableau.cdt.ca.gov/views/StateDashboard-CleanSources/11_1TierAssignmentMap?:origin=card_share_link&:embed=n');
+
+// these are their own toggle sets
+let ethnicityGroupChart = displayChart('#ethnicityGroupChartContainer', 1000, 327, 'https://tableau.cdt.ca.gov/views/StateDashboard-CleanSources/12_1Ethnicity?:origin=card_share_link&:embed=n')
+let genderGroupChart = displayChart('#genderGroupChartContainer', 1000, 327, 'https://tableau.cdt.ca.gov/views/StateDashboard-CleanSources/12_2Gender?:origin=card_share_link&:embed=n')
+let ageGroupChart = displayChart('#ageGroupChartContainer', 1000, 327, 'https://tableau.cdt.ca.gov/views/StateDashboard-CleanSources/12_3Age?:origin=card_share_link&:embed=n')
+
 function resetToggles() {
   togglers.forEach(toggle => {
     toggle.classList.remove('toggle-active')  
@@ -25,42 +61,3 @@ togglers.forEach(toggle => {
     this.classList.add('toggle-active');
   })
 })
-
-
-/*
-// This is example chart switching code Avra demonstrated:
-
-var containerDiv = document.getElementById("tableauViz");
-var url = "https://public.tableau.com/views/Refugees_test/Dashboard2?:language=en&:display_count=y&publish=yes&:toolbar=n&:origin=viz_share_link";
-var options = {
-  width: "1200px",
-  height: "900px",
-  "Continent": "",
-  onFirstInteractive: function () {
-    activeSheet = viz.getWorkbook().getActiveSheet().getWorksheets()[1];
-  }
-}
-var viz = new tableau.Viz(containerDiv, url, options);
-activeSheet.applyFilterAsync("Continent", continent, tableau.FilterUpdateType.REPLACE);
-
-These are parameters for the county charts:
-
-
-County
-
-Cases and Deaths
-
-<param name='host_url' value='https%3A%2F%2Ftableau.cdt.ca.gov%2F' />  
-<param name='name' value='StateDashboard-CleanSources&#47;3_1County-Reported' />
-
-Hospitals
-
-<param name='host_url' value='https%3A%2F%2Ftableau.cdt.ca.gov%2F' />  
-<param name='name' value='StateDashboard-CleanSources&#47;9_1CountyHosp' />
-
-Testing
-
-<param name='host_url' value='https%3A%2F%2Ftableau.cdt.ca.gov%2F' />  
-<param name='name' value='StateDashboard-CleanSources&#47;6_1CountyTesting' />
-
-*/
