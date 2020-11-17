4353bf7858ca5e7a8f6d818bc11fd4e0902d80b7
carterm <carter.medlin@gmail.com>
2020-11-17 15:37:15 -0800

    update test

---
+++ pages/_data/tierDates.js
@@ -0,0 +1,14 @@
+const caseStats = require('./tableauCovidMetrics.json');
+
+const data = {
+  summary: {
+    totalCases: caseStats[0]['TOTALCONFIRMED'],
+    totalCasesIncrease: caseStats[0]['TOTALCONFIRMED_DAILYPCTCHG'] * 100,
+    totalDeaths: caseStats[0]['NUMBERDIED'],
+    totalDeathsIncrease: caseStats[0]['NUMBERDIED_DAILYPCTCHG'] * 100,
+    testsReported: caseStats[0]['TESTED'],
+    lastStatsDate: caseStats[0]['DATE'] + 'T19:00:00Z'
+  }
+};
+
+module.exports = data;
