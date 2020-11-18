1e832a8492da9a71aed114c6188757a624f48e8f
carterm <carter.medlin@gmail.com>
2020-11-17 16:40:42 -0800

    pre-translating new tier location

--- pages/_data/tierData.js
+++ pages/_data/tierData.js
@@ -4,4 +4,4 @@ module.exports = jsonFile.Table2[0];
 
 
 //11ty usage
-//{%-set _varTierEndDate_ = data.TIER_ENDDATE | formatDate2(false, tags) -%}
+//{%-set _varTierEndDate_ = tierData.TIER_ENDDATE | formatDate2(false, tags) -%}
