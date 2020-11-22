0abfbd5d1260aee3890aaf3dda4fc7e7b55d4ee1
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-11-22 11:51:50 -0800

    now getting 100 on lighthouse a11y audit

--- src/js/equity-dash/search/template.js
+++ src/js/equity-dash/search/template.js
@@ -3,11 +3,10 @@ export default function template(countyLabel, countyPlaceholder) {
     <div class="row d-flex justify-content-md-center">
       <form class="form-inline" id="county-form">
         <div class="form-group mx-sm-3 mb-2 pos-rel autocomplete-inline">
-          <label for="county" class="sr-only">County</label>
+          <label for="location-query" class="sr-only">County</label>
           <div class="awesomplete">
             <input aria-expanded="false" aria-owns="awesomplete_list_1" autocomplete="off" class="form-control form-control-lg" data-list="" data-multiple="" id="location-query" role="combobox" type="text" placeholder="Enter a county">
             <button class="clear d-none" id="clearCounty"><span class="ca-gov-icon-close-line" aria-hidden="true"></span> <span class="underline">Clear</span></button>
-            <ul hidden="" role="listbox" id="awesomplete_list_1"></ul>
             <span class="visually-hidden" role="status" aria-live="assertive" aria-atomic="true">Type 2 or more characters for results.</span>
           </div>
         </div>
