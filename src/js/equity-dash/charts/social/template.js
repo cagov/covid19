613825b8ea9099eaad252865f574e632363a0b17
Chacha Sikes <chachasikes@gmail.com>
2020-11-22 21:14:13 -0800

    Fix bootstrap responsive containers and chart data label so that it lines up with missingness & more closely matches the design. Also move Statewide case rate label to the left side of the chart, and fix the cut off K in the chart legend.

--- src/js/equity-dash/charts/social/template.js
+++ src/js/equity-dash/charts/social/template.js
@@ -1,11 +1,12 @@
+import css from './index.scss';
+
 export default function template(translationsObj) {
-  return /*html*/`<div class="bg-lightblue py-2 full-bleed">
+  return /*html*/`<div class="py-2 bg-lightblue full-bleed">
     <div class="container">
     <h2 class="text-center">${translationsObj.sectionTitle}</h2>
     <p>${translationsObj.sectionDescription}</p>
     <div class="col-lg-12 bg-white px-3 py-4">
         <div class="row d-flex justify-content-md-center">
-
             <div class="inline-toggle-link-container">
                 <div class="toggle-links bg-darkblue bd-darkblue">
                   <a href="#" class="toggle-active js-toggle-group income">${translationsObj.chartButtonIncome}</a><a href="#" class="js-toggle-group  housing">${translationsObj.chartButtonHousing}</a><a href="#" class="js-toggle-group healthcare">${translationsObj.chartButtonHousing}</a>
@@ -13,7 +14,7 @@ export default function template(translationsObj) {
             </div>
         </div>
         <div class="row">
-            <div class="col-10 mx-auto">
+            <div class="col-lg-9 col-md-9 col-sm-12 mx-auto">
               <div class="chart-title">${translationsObj.chartTitleIncome}</div>
               <div class="svg-holder">
                 <div class="bar-overlay">an empty tooltip</div>
@@ -21,7 +22,9 @@ export default function template(translationsObj) {
             </div>
         </div>
         <div class="row d-flex justify-content-md-center">
-          <p class="chart-data-label">${translationsObj.footnote}</p>
+          <div class="col-lg-9 col-md-9 col-sm-12 mx-auto">
+            <p class="chart-data-label col-lg-10 mx-auto">${translationsObj.footnote}</p>
+          </div>
         </div>
     </div><!--END col-12-->
   </div><!--END CONTAINER-->
