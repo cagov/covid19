52a62e01359a02ca282aec39f88f3cf87c8d61d6
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-12-29 10:51:16 -0800

    add file to prevent purge

---
+++ src/js/accordion/template.js
@@ -0,0 +1,7 @@
+// The accordion makeup is created from WordPress hooks and transformed into the desired custom element by the 11ty build. The desired markup contains this one new class which we need not to be purged
+
+export default function template() {
+  return /*html*/`
+    <div class="plus-munus"
+  `
+}
