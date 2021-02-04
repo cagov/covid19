8405d508f21f9819ebb612e9ac33a66cebaa1336
Jim Bumgardner <jbum@jbum.com>
2021-02-04 10:09:00 -0800

    Latest chart sandbox fixes.

--- src/js/charts-sandbox/charts/ve-race-ethnicity-age/template.js
+++ src/js/charts-sandbox/charts/ve-race-ethnicity-age/template.js
@@ -7,11 +7,7 @@ export default function template(translationsObj) {
 
         <div class="row">
           <div class="col-lg-9 col-md-9 col-sm-12 mx-auto px-0">
-<<<<<<< HEAD
-            <div class="chart-title">TITLE</div>
-=======
             <div class="chart-title">Vaccinations by Race / Ethnicity and Age</div>
->>>>>>> jbum_charts_sandbox
             <div class="svg-holder">
             </div>
           </div>
@@ -19,11 +15,7 @@ export default function template(translationsObj) {
 
         <div class="row">
           <div class="col-lg-9 col-md-9 col-sm-12 mx-auto px-0">
-<<<<<<< HEAD
-            <p class="chart-data-label col-lg-10 mx-auto">FOOTNOTE</p>
-=======
             <p class="chart-data-label col-lg-10 mx-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
->>>>>>> jbum_charts_sandbox
           </div>
         </div>
       </div><!--END col-12-->
