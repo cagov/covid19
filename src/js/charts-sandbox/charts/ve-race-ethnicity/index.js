2b8d7c03180f7760e193644f9fb3856223b42974
Jim Bumgardner <jbum@jbum.com>
2021-02-09 15:17:05 -0800

    Initial county search implementation.

--- src/js/charts-sandbox/charts/ve-race-ethnicity/index.js
+++ src/js/charts-sandbox/charts/ve-race-ethnicity/index.js
@@ -149,7 +149,8 @@ class CAGOVEquityVaccinesRaceEthnicity extends window.HTMLElement {
       function (e) {
         console.log("X County selected",e.detail.filterKey);
         this.county = e.detail.county;
-        let searchURL = this.chartOptions.dataUrlCounty.replace("<county>",this.county)
+        // let searchURL = this.chartOptions.dataUrlCounty.replace("<county>",this.county.toLowerCase().replace(/ /g, ""))
+        let searchURL = this.chartOptions.dataUrlCounty.replace("<county>",'california')
         this.retrieveData(searchURL);
         // this.resetTitle();
       }.bind(this),
