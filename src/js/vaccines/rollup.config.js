65138d70c3e2bf279f084a9a40bbcbd328f5343f
Chacha Sikes <chachasikes@gmail.com>
2021-02-12 09:56:09 -0800

    add vaccines code

---
+++ src/js/vaccines/rollup.config.js
@@ -0,0 +1,36 @@
+import resolve from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
+import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
+
+const defaultConfig = {
+  equityChartsSampleDataLoc: 'https://files.covid19.ca.gov/data/chart-sandbox/',
+  equityChartsVEDataLoc: 'https://files.covid19.ca.gov/data/vaccine-equity/',
+}
+const stagingConfig =  {
+  equityChartsSampleDataLoc: 'https://files.covid19.ca.gov/data/chart-sandbox/',
+  equityChartsVEDataLoc: 'https://files.covid19.ca.gov/data/vaccine-equity/',
+}
+
+const devOutputPath = 'docs/js/vaccines.js';
+const prodOutputPath = 'pages/_buildoutput/vaccines.js';
+const outputPath = (process.env.NODE_ENV === 'development') ? devOutputPath : prodOutputPath;
+const jsConfig = (process.env.NODE_ENV === 'staging' || process.env.NODE_ENV == "development") ? stagingConfig : defaultConfig;
+
+export default {
+  input: 'src/js/vaccines/index.js',
+  output: {
+    intro: 'const config = '+JSON.stringify(jsConfig),
+    file: outputPath,
+    format: 'esm'
+  },
+  plugins: [
+    resolve(), 
+    postcss({
+      extract: false,
+      modules: false,
+      use: ['sass'],
+    }),
+    // terser(),
+    json()]
+};
