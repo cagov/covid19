423111ab2d38b2214a3bb898a3e892ce517a4aa7
Chacha Sikes <chachasikes@gmail.com>
2021-02-12 10:45:53 -0800

    add code that enables vaccines charts web components

--- src/js/rollup.config.all.js
+++ src/js/rollup.config.all.js
@@ -8,6 +8,7 @@ import video from './video/rollup.config';
 import dashboard from './dashboard/rollup.config';
 import dashboard2 from './dashboard-v2/rollup.config';
 import equitydash from './equity-dash/rollup.config';
+import vaccines from './vaccines/rollup.config';
 
 import fs from 'fs';
 import path from 'path';
@@ -47,6 +48,7 @@ export default [
   ...(shouldIRebuild('/dashboard/', '../../docs/js/dashboard.js') ? [dashboard] : []),
   ...(shouldIRebuild('/dashboard-v2/', '../../docs/js/dashboard-v2.js') ? [dashboard2] : []),
   ...(shouldIRebuild('/equity-dash/', '../../docs/js/equitydash.js') ? [equitydash] : [equitydash]),
+  ...(shouldIRebuild('/vaccines/', '../../docs/js/vaccines.js') ? [vaccines] : [vaccines]),
   esm,
   // Don't include ES5 file in dev mode.
   ...((process.env.NODE_ENV === 'development') ? [] : [es5])
