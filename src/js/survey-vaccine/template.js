94b9f9b7a9d8378217a5970f82ab25ed2361c65d
kkoryaka <31669748+kkoryaka@users.noreply.github.com>
2021-02-25 16:37:14 -0800

    added aria-label and landmarks to alert components (ccg-1559)

--- src/js/survey-vaccine/template.js
+++ src/js/survey-vaccine/template.js
@@ -10,9 +10,9 @@ export default function (incomingPrompt) {
   return `<div role="alert">
     <div class="survey">
       <div class="survey-content">
-        <p class="survey-content-prompt">${surveyPrompt}</p>
-        <a href="${surveyUrl}" target="_new" class="btn-link pt-2 pb-2 px-4 js-goto-survey"><span class="sr-only">Open survey </span><span aria-hidden="true">OK</span></a> 
-        <button class="btn-link pt-2 pb-2 px-4 js-dismiss-survey" type="button" aria-label="Close" data-dismiss="alert"><span aria-hidden="true">Dismiss</span></button>
+        <p class="survey-content-prompt" role="region" aria-label="${surveyPrompt}, select below">${surveyPrompt}</p>
+        <a href="${surveyUrl}" target="_new" class="btn-link pt-2 pb-2 px-4 js-goto-survey" role="region" aria-label="Ok, open survey"><span aria-hidden="true">OK</span></a> 
+        <button class="btn-link pt-2 pb-2 px-4 js-dismiss-survey" role="region" aria-label="Dismiss, close" data-dismiss="alert">Dismiss</button>
       </div>
     </div>
   </div>`;
--- src/js/survey/template.js
+++ src/js/survey/template.js
@@ -10,9 +10,9 @@ export default function (incomingUrl, incomingPrompt) {
   return `<div role="alert">
     <div class="survey">
       <div class="survey-content">
-        <p class="survey-content-prompt">${surveyPrompt}</p>
-        <a href="${surveyUrl}" target="_new" class="btn-link pt-2 pb-2 px-4 js-goto-survey"><span class="sr-only">Open survey </span><span aria-hidden="true">OK</span></a> 
-        <button class="btn-link pt-2 pb-2 px-4 js-dismiss-survey" type="button" aria-label="Close" data-dismiss="alert"><span aria-hidden="true">Dismiss</span></button>
+        <p class="survey-content-prompt" role="region" aria-label="${surveyPrompt}, choose below">${surveyPrompt}</p>
+        <a href="${surveyUrl}" target="_new" class="btn-link pt-2 pb-2 px-4 js-goto-survey" role="region" aria-label="Ok, go to survey">OK</a> 
+        <button class="btn-link pt-2 pb-2 px-4 js-dismiss-survey" role="region" aria-label="Close, dismiss" data-dismiss="alert">Dismiss</button>
       </div>
     </div>
   </div>`;
