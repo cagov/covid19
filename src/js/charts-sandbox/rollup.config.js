d0cff20871f92c2817425ed40803bc3c31450f36
Jim Bumgardner <jbum@jbum.com>
2021-03-02 17:11:55 -0800

    Added new chart to sandbox.

--- src/js/charts-sandbox/rollup.config.js
+++ src/js/charts-sandbox/rollup.config.js
@@ -6,26 +6,16 @@ import { terser } from 'rollup-plugin-terser';
 const defaultConfig = {
   equityChartsSampleDataLoc: 'https://files.covid19.ca.gov/data/chart-sandbox/',
   equityChartsVEDataLoc: 'https://files.covid19.ca.gov/data/vaccine-equity/',
-<<<<<<< HEAD
-=======
   chartsVHPIDataLoc: 'https://files.covid19.ca.gov/data/vaccine-hpi/',
->>>>>>> jbum_hpi_chart
 }
 const stagingConfig =  {
   equityChartsSampleDataLoc: 'https://files.covid19.ca.gov/data/chart-sandbox/',
   equityChartsVEDataLoc: 'https://files.covid19.ca.gov/data/vaccine-equity/',
-<<<<<<< HEAD
-}
-
-const devOutputPath = 'docs/js/chartssandbox.js';
-const prodOutputPath = 'pages/_buildoutput/chartssandbox.js';
-=======
   chartsVHPIDataLoc: 'https://files.covid19.ca.gov/data/vaccine-hpi/',
 }
 
 const devOutputPath = 'docs/js/charts-sandbox.js';
 const prodOutputPath = 'pages/_buildoutput/charts-sandbox.js';
->>>>>>> jbum_hpi_chart
 const outputPath = (process.env.NODE_ENV === 'development') ? devOutputPath : prodOutputPath;
 const jsConfig = (process.env.NODE_ENV === 'staging' || process.env.NODE_ENV == "development") ? stagingConfig : defaultConfig;
 
