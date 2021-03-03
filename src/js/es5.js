d6455747445985f7c08cf375b0362ebf7879babe
Jim Bumgardner <jbum@jbum.com>
2021-03-02 17:05:45 -0800

    Initial HPI people chart.

--- src/js/es5.js
+++ src/js/es5.js
@@ -15,6 +15,7 @@ import './equity-dash/index.js';
 import './equity-dash/charts/ie11.scss';
 import './vaccines/index.js';
 import './vaccines/charts/ie11.scss';
+import './charts-sandbox/index.js';
 import './dashboard-v2/index.js';
 
 import applyAccordionListeners from './tracking-you/index.js';
