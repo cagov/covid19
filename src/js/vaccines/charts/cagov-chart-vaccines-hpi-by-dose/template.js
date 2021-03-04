aeed17f8dd523cf2a8b630d413b27be4e37837da
aaronhans <aaronhans@gmail.com>
2021-03-03 21:02:33 -0800

    remove some classes

--- src/js/vaccines/charts/cagov-chart-vaccines-hpi-by-dose/template.js
+++ src/js/vaccines/charts/cagov-chart-vaccines-hpi-by-dose/template.js
@@ -14,24 +14,18 @@ export default function template({
   footerDisplayText = "Updated {PUBLISHED_DATE} with data from {LATEST_ADMINISTERED_DATE}"
 }) {
   return /*html*/ `
-    <div class="py-2">
-    <div class="container">
-      <div class="col-lg-12 bg-white py-4">
-        <div class="row">
-          <div class="col-lg-9 col-md-9 col-sm-12 mx-auto px-0">
-            <div class="chart-title">${chartTitle}</div>
-            <div class="small-text">${chartDescription}</div>
-            <div class="svg-holder"></div>
-          </div>
-        </div>
-        ${footerDisplayText !== null ? `<div class="row">
-        <div class="col-lg-9 col-md-9 col-sm-12 mx-auto px-0 pt-2">
-          <p class="chart-data-label small-text">${footerDisplayText}</p>
-        </div>
-      </div>` : ""}
-      </div><!--END col-12-->
-    </div><!--END CONTAINER-->
-  </div>`;
+    <div class="row py-4">
+      <div class="col-lg-9 col-md-9 col-sm-12 mx-auto">
+        <div class="chart-title">${chartTitle}</div>
+        <div class="small-text">${chartDescription}</div>
+        <div class="svg-holder"></div>
+      </div>
+    </div>
+    ${footerDisplayText !== null ? `<div class="row">
+    <div class="col-lg-9 col-md-9 col-sm-12 mx-auto pt-2">
+      <p class="chart-data-label small-text">${footerDisplayText}</p>
+    </div>
+  </div>` : ""}`;
 }
 
 
