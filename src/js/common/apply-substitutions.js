d083480e3483ed443b4faa8118199791d2ce9701
Jim Bumgardner <jbum@jbum.com>
2021-03-04 13:44:56 -0800

    Removed double brackets for now.

--- src/js/common/apply-substitutions.js
+++ src/js/common/apply-substitutions.js
@@ -2,10 +2,12 @@
 */
 
 export default function applySubstitutions(textString, substitutions) {
+    console.log("BEFORE",textString);
     for (const key in substitutions) {
         const value = substitutions[key];
         textString = textString.replace('{{' + key + '}}', value);
         textString = textString.replace('{' + key + '}', value);
     }
+    console.log("AFTER",textString);
     return textString;
 }
