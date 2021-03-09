6a61fb57723a1c1914148f6a616e85b7bafccba4
kkoryaka <31669748+kkoryaka@users.noreply.github.com>
2021-03-09 14:59:13 -0800

    made pdf links to add pdf icons automatically based on file extension (ccg-1731)

---
+++ src/js/links/index.js
@@ -0,0 +1,17 @@
+// Adding pdf span to the links with class pdf
+var pdf = '<span class="pdf-link-icon no-underline-icon" aria-hidden="true">PDF</span><span class="sr-only"> (this is a pdf file)</span>';
+
+// selector is looking for links with pdf extension in the href
+var pdfLink = document.querySelectorAll("a[href*='.pdf']");
+for (var i=0; i < pdfLink.length; i++) {
+  pdfLink[i].innerHTML+=pdf; // += concatenates to pdf links
+}
+
+
+// Adding external link icon to the links with class external
+var ext = '<span class="ca-gov-icon-external-link link-icon" aria-hidden="true"></span>';
+
+var externalLink = document.querySelectorAll(".external");
+for (var i=0; i < externalLink.length; i++) {
+  externalLink[i].innerHTML += ext; // += cconcatenates to external links
+}
