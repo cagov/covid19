564f3fddbc542467c48df470bdf2f0f0b0f90e5b
kkoryaka <31669748+kkoryaka@users.noreply.github.com>
2021-03-09 14:16:28 -0800

    cleaned up double L in the code

--- src/js/links/index.js
+++ src/js/links/index.js
@@ -2,16 +2,16 @@
 var pdf = '<span class="pdf-link-icon no-underline-icon" aria-hidden="true">PDF</span><span class="sr-only"> (this is a pdf file)</span>';
 
 // selector is looking for links with pdf extension in the href
-var pdflLink = document.querySelectorAll("a[href*='.pdf']");
-for (var i=0; i < pdflLink.length; i++) {
-  pdflLink[i].innerHTML+=pdf; // += concatenates to pdf links
+var pdfLink = document.querySelectorAll("a[href*='.pdf']");
+for (var i=0; i < pdfLink.length; i++) {
+  pdfLink[i].innerHTML+=pdf; // += concatenates to pdf links
 }
 
 
 // Adding external link icon to the links with class external
 var ext = '<span class="ca-gov-icon-external-link link-icon" aria-hidden="true"></span>';
 
-var externallLink = document.querySelectorAll(".external");
-for (var i=0; i < externallLink.length; i++) {
-  externallLink[i].innerHTML += ext; // += cconcatenates to external links
+var externalLink = document.querySelectorAll(".external");
+for (var i=0; i < externalLink.length; i++) {
+  externalLink[i].innerHTML += ext; // += cconcatenates to external links
 }
