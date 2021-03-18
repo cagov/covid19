974722ffc073c7d70fb6302feeedf1a1f14841fc
Jim Bumgardner <jbum@jbum.com>
2021-03-18 13:25:11 -0700

    Margin fix to match.

--- src/js/equity-dash/charts/social-determinants/template.js
+++ src/js/equity-dash/charts/social-determinants/template.js
@@ -2,8 +2,10 @@
 import css from './index.scss';
 
 export default function template(translationsObj) {
-  return /*html*/`<div class="py-2 bg-lightblue full-bleed">
-    <div class="container col-lg-9">
+  return /*html*/`<div class="py-2 bg-lightblue full-bleed px-4">
+    <div class="container">
+    <div class="row">
+    <div class="container col-lg-10 mx-auto">
     <h2 class="text-center" id="factors">${translationsObj.sectionTitle}</h2>
     <p>${translationsObj.sectionDescription}</p>
     <div class="col-lg-12 bg-white px-3 py-4">
@@ -15,7 +17,7 @@ export default function template(translationsObj) {
           </div>
         </div>
         <div class="row">
-          <div class="col-lg-7 col-md-9 col-sm-12 mx-auto px-0">
+          <div class="col-lg-9 col-md-9 col-sm-12 mx-auto px-0">
               <div class="chart-title">${translationsObj.chartTitleIncome}</div>
               <div class="svg-holder">
                 <div class="tooltip-container">an empty tooltip</div>
@@ -29,5 +31,7 @@ export default function template(translationsObj) {
         </div>
     </div><!--END col-12-->
   </div><!--END CONTAINER-->
+  </div> <!-- row -->
+  </div> <!-- container -->
 </div><!--END BG lightblue-->`;
 }
