6a995c5a0ad6101d407f87e70f60ce2687f72b1d
kkoryaka <31669748+kkoryaka@users.noreply.github.com>
2021-03-23 16:14:03 -0700

    updated back-to-top button scrolling direction  behavior

--- src/js/gototop/index.js
+++ src/js/gototop/index.js
@@ -36,26 +36,45 @@ function addBackToTopButon() {
 // If an user scrolls down the page for more than 400px activate back to top button
 // othervise keep it invisible
 var timer;
-window.onscroll = function () {
 
-  var returnTopButton = document.querySelector(".return-top");
-  if (document.body.scrollTop >= 400 || document.documentElement.scrollTop >= 400) {
-    // ass soon as user staarts scrolling back to top will appear
-    if(timer != "undefined"){
-      clearTimeout(timer);
-    }
-    returnTopButton.classList.add("is-visible");
-    
-    timer = setTimeout(function(){
-      
+var lastScrollTop = 0;
+
+window.addEventListener("scroll", function() { 
+var returnTopButton = document.querySelector(".return-top");
+var st = window.pageYOffset || document.documentElement.scrollTop; 
+   if (st > lastScrollTop) {
+      // downscroll code
       returnTopButton.classList.remove("is-visible");
-  
-    },2000) //Back to top removes itself after 2 sec of inactivity
-  }
-  else {
-    returnTopButton.classList.remove("is-visible");
+
+   } else {
+      // upscroll code
+      if (document.body.scrollTop >= 400 || document.documentElement.scrollTop >= 400) {
+        if (timer != "undefined"){
+          clearTimeout(timer);
+        }
+        returnTopButton.classList.add("is-visible");
+        
+        timer = setTimeout(function(){
+          
+          returnTopButton.classList.remove("is-visible");
+      
+        },2000) //Back to top removes itself after 2 sec of inactivity
+      }
+    // bottom of the page 
+
+      else {
+        returnTopButton.classList.remove("is-visible");
+      }
+   }
+   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);
+
+
+// Hittin' rock bottom
+window.onscroll = function(ev) {
+  var returnTopButton = document.querySelector(".return-top");
+  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+    returnTopButton.classList.add("is-visible");
+    clearTimeout(timer);
   }
 };
-
-
-
