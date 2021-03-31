eac8bb5c4242aff2d4f0da01885fd3efffa13a37
Jim Bumgardner <jbum@jbum.com>
2021-03-31 12:09:03 -0700

    Work in progress.

--- src/js/dashboard-v2/charts/cagov-chart-dashboard-confirmed-cases-episode-date/template.js
+++ src/js/dashboard-v2/charts/cagov-chart-dashboard-confirmed-cases-episode-date/template.js
@@ -7,17 +7,17 @@
 export default function template({
   chartTitle = "Confirmed cases by age in California",
   chartDataLabel = null,
-  chartLegend1 = null,
-  chartLegend2Increase = null,
 }) {
   return /*html*/ `
     <div class="py-2">
       <div class="bg-white pt-2 pb-1">
         <div class="mx-auto">
             <div class="chart-title">${chartTitle}</div>
+            <!-- tabs go here -->
             <div class="chart-header">
-            <div class="header-line"><span class="big-num">2,6236</span> COVID-19 hospitalized patients </div>
-            <div class="header-line"><span class="med-num">+24</span> more patients (0.9% increase) </div>
+            <div class="header-line"><span class="big-num">3,568,426</span> total confirmed cases </div>
+            <div class="header-line"><span class="med-num">1,962</span> new cases (0.1% increase) </div>
+            <div class="header-line"><span class="med-num">4.6</span> cases per 100K (7-day average) </div>
             </div>
             <div class="svg-holder"></div>
         </div>
