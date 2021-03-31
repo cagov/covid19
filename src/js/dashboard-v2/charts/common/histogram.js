9acb1739bb1085a7ba6e0a555773cef04bd2edd1
Jim Bumgardner <jbum@jbum.com>
2021-03-31 16:38:23 -0700

    Chart tweaks. Added pending block and captions.

--- src/js/dashboard-v2/charts/common/histogram.js
+++ src/js/dashboard-v2/charts/common/histogram.js
@@ -1,15 +1,6 @@
 // generic histogram chart, as used on top of state dashboard
 
-
-/**
- * Draw line element
- * @param {*} svg 
- * @param {*} data 
- * @param {*} x 
- * @param {*} y 
- * @param {*} rootID 
- */
-function writeLine(svg, data, x, y, rootID='barid') {
+function writeLine(svg, data, x, y, { rootID='barid' }) {
   let max_y_domain = y.domain()[1];
   let max_x_domain = x.domain()[1];
   let component = this;
@@ -24,15 +15,7 @@ function writeLine(svg, data, x, y, rootID='barid') {
         );
 }
 
-/**
- * Draw bars elements
- * @param {*} svg 
- * @param {*} data 
- * @param {*} x 
- * @param {*} y 
- * @param {*} rootID 
- */
- function writeBars(svg, data, x, y, rootID='barid') {
+ function writeBars(svg, data, x, y, { rootID='barid' }) {
     let groups = svg.append("g")
       .attr("class","fg-bars")
       .selectAll("g")
@@ -50,17 +33,41 @@ function writeLine(svg, data, x, y, rootID='barid') {
         .attr("id", (d, i) => rootID+'-'+i);
 }
 
-/**
- * Draw X Axis with dates
- * @param {*} svg 
- * @param {*} data 
- * @param {*} x 
- * @param {*} y 
- * @param {*} rootID 
- */
 
+function writePendingBlock(svg, data, x, y,
+  { pending_date=null,
+    pending_legend=null,
+    rootID='barid'} ) {
+    let xgroup = svg.append("g")
+      .attr("class",'pending-block');
 
-function writeDateAxis(svg, data, x, y, rootID='barid') {
+    const max_x_domain = x.domain()[1];
+    const min_y_domain = y.domain()[0];
+    const max_y_domain = y.domain()[1];
+    let nbr_pending = 0;
+    for (let i = 0; i < data.length; ++i) {
+      nbr_pending += 1
+      if (data[i].DATE == pending_date) {
+        break;
+      }
+    }
+    console.log("PENDING_DATE",pending_date);
+    console.log("Pending",nbr_pending);
+
+    xgroup.append('rect')
+      .attr("x",x(nbr_pending))
+      .attr("y",y(max_y_domain))
+      .attr("width",x(0) - x(nbr_pending))
+      .attr("height",y(min_y_domain)-y(max_y_domain));
+    xgroup.append('text')
+      .text(pending_legend)
+      .attr("x",x(0))
+      .attr("y",y(max_y_domain)-2);
+}
+
+function writeDateAxis(svg, data, x, y, 
+  { x_axis_legend=null,
+    rootID='barid'} ) {
   const tick_height = 4;
   const tick_upper_gap = 1;
   const tick_lower_gap = 2;
@@ -88,10 +95,20 @@ function writeDateAxis(svg, data, x, y, rootID='barid') {
       }
     }
   });
+  if (x_axis_legend) {
+    xgroup.append('text')
+    .attr('class','x-axis-legend')
+    .text(x_axis_legend)
+    .attr("x", this.dimensions.width)
+    .attr("y", this.dimensions.height-4) // +this.getYOffset(i)
+  }
 
 }
 
-function writeLeftYAxis(svg, data, x, y, y_div=20, rootID='barid') {
+function writeLeftYAxis(svg, data, x, y, 
+                        { y_div=20, 
+                          y_axis_legend=null,
+                          rootID='barid' }) {
   let ygroup = svg.append("g")
       .attr("class",'left-y-axis');
   const max_y_domain = y.domain()[1];
@@ -123,15 +140,25 @@ function writeLeftYAxis(svg, data, x, y, y_div=20, rootID='barid') {
       .attr("x", x(max_x_domain)-tick_left_gap)
       .attr("y", y(yi)) // +this.getYOffset(i)
   }
+  if (y_axis_legend) {
+    ygroup.append('text')
+    .attr('class','left-y-axis-legend')
+    .text(y_axis_legend)
+    .attr("x", 0)
+    .attr("y", 0) // +this.getYOffset(i)
+  }
 }
 
-function writeRightYAxis(svg, data, x, y, y_div=20, rootID='barid') {
+function writeRightYAxis(svg, data, x, y, 
+                        { y_div=20, 
+                          y_axis_legend=null,
+                          rootID='barid' }) {
   let ygroup = svg.append("g")
       .attr("class",'right-y-axis');
   const max_y_domain = y.domain()[1];
   const min_x_domain = x.domain()[0];
-  const tick_right_gap = 10;
-  const tick_length = 4;
+  const tick_left_gap = 10;
+  const tick_right_gap = 30;
 
   console.log("Drawing Right Axis  max_y_domain",max_y_domain,rootID);
 
@@ -150,7 +177,7 @@ function writeRightYAxis(svg, data, x, y, y_div=20, rootID='barid') {
       .attr('class','y-tick');
 
     subg.append('line')
-      .attr('x1', x(min_x_domain))
+      .attr('x1', x(min_x_domain)+tick_left_gap)
       .attr('y1', y(yi))
       .attr('x2', x(min_x_domain)+tick_right_gap)
       .attr('y2', y(yi));
@@ -159,6 +186,13 @@ function writeRightYAxis(svg, data, x, y, y_div=20, rootID='barid') {
       .text(y_caption)
       .attr("x", x(min_x_domain)+tick_right_gap)
       .attr("y", y(yi)) // +this.getYOffset(i)
+  }
+  if (y_axis_legend) {
+    ygroup.append('text')
+    .attr('class','right-y-axis-legend')
+    .text(y_axis_legend)
+    .attr("x", this.dimensions.width)
+    .attr("y", 0) // +this.getYOffset(i)
   }
 }
 
@@ -175,6 +209,12 @@ function writeRightYAxis(svg, data, x, y, y_div=20, rootID='barid') {
     line_date_offset = 0,
     left_y_div = 1000,
     right_y_div = 0,
+    left_y_axis_legend = null,
+    right_y_axis_legend = null,
+    line_legend = null,
+    x_axis_legend = null,
+    pending_date = null,
+    pending_legend = null,
     root_id = "barid" } )  {
     // // this statement produces an array of strings in IE11 and an array of numbers in modern browsers
     // let categories = data.map((group) => group.CATEGORY);
@@ -244,28 +284,41 @@ function writeRightYAxis(svg, data, x, y, y_div=20, rootID='barid') {
     this.svg.selectAll("g").remove();
 
     if (time_series_key_bars) {
-      writeBars.call(this, this.svg, chartData.time_series[time_series_key_bars], this.xbars, this.ybars, root_id);
+      writeBars.call(this, this.svg, chartData.time_series[time_series_key_bars], this.xbars, this.ybars, 
+        { root_id:root_id});
       // bar legend on left
     }
     if (time_series_key_line) {
-      writeLine.call(this, this.svg, chartData.time_series[time_series_key_line], this.xline, this.yline, root_id);
+      writeLine.call(this, this.svg, chartData.time_series[time_series_key_line], this.xline, this.yline, 
+        { line_legend:line_legend, root_id:root_id});
       // line legend on right or left, depending on whether bar is provided
     }
-    if (time_series_key_bars) {
-      writeDateAxis.call(this, this.svg, chartData.time_series[time_series_key_bars], this.xbars, this.ybars);
-    } else if (time_series_key_line) {
-      writeDateAxis.call(this, this.svg, chartData.time_series[time_series_key_line], this.xline, this.yline);
+
+    if (pending_date && pending_legend) {
+      writePendingBlock.call(this, this.svg, chartData.time_series[time_series_key_bars], this.xbars, this.ybars, 
+            { root_id:root_id, pending_date:pending_date, pending_legend:pending_legend});
     }
 
+    if (time_series_key_bars) {
+      writeDateAxis.call(this, this.svg, chartData.time_series[time_series_key_bars], this.xbars, this.ybars,
+          {x_axis_legend:x_axis_legend, root_id:root_id} );
+    } else if (time_series_key_line) {
+      writeDateAxis.call(this, this.svg, chartData.time_series[time_series_key_line], this.xline, this.yline,
+        {x_axis_legend:x_axis_legend, root_id:root_id} );
+    }
     // Write Y Axis, favoring line on left, bars on right
     if (time_series_key_line) {
-      writeLeftYAxis.call(this, this.svg, chartData.time_series[time_series_key_line], this.xline, this.yline, left_y_div, root_id);
+      writeLeftYAxis.call(this, this.svg, chartData.time_series[time_series_key_line], this.xline, this.yline,
+         {y_div:left_y_div, y_axis_legend: left_y_axis_legend, root_id:root_id});
       if (time_series_key_bars) {
-        if (right_y_div)
-          writeRightYAxis.call(this, this.svg, chartData.time_series[time_series_key_bars], this.xbars, this.ybars, right_y_div, root_id);      }      
+        if (right_y_div) {
+          writeRightYAxis.call(this, this.svg, chartData.time_series[time_series_key_bars], this.xbars, this.ybars, 
+              {y_div:right_y_div, y_axis_legend: right_y_axis_legend, root_id:root_id});
+        }
+      }
     } else if (time_series_key_bars) {
-      writeLeftYAxis.call(this, this.svg, chartData.time_series[time_series_key_bars], this.xbars, this.ybars, left_y_div, root_id);
-
+      writeLeftYAxis.call(this, this.svg, chartData.time_series[time_series_key_bars], this.xbars, this.ybars, 
+          { y_div:left_y_div, y_axis_legend: left_y_axis_legend, root_id:root_id});
     }
 
     // writeLegend.call(this, this.svg, data, this.xbars, this.y, baselineData);
