f4c60bb17c1a698c62f296431cb3a9521c4c0727
Jim Bumgardner <jbum@jbum.com>
2021-03-31 14:14:14 -0700

    Latest chart fixes.

--- src/js/dashboard-v2/charts/common/histogram.js
+++ src/js/dashboard-v2/charts/common/histogram.js
@@ -1,94 +1,22 @@
 // generic histogram chart, as used on top of state dashboard
 
+
 /**
- * Primary chart rendering function
- * @param {object} svg 
- * @param {object} data 
- * @param {number} x 
- * @param {number} y 
+ * Draw line element
+ * @param {*} svg 
+ * @param {*} data 
+ * @param {*} x 
+ * @param {*} y 
+ * @param {*} rootID 
  */
- function writeLegend(svg, data, x, y) {
-    // Build legend.
-    // const legendText = this.getLegendText();
-    // if (legendText.length == 0) {
-    //   return;
-    // }
-    // const legendW = y.bandwidth()*1.2;
-    // const legendY =  this.dimensions.margin.top/2;
-    // const legend2X = this.dimensions.width/2;
-
-    // let group = svg.append("g");
-
-
-    // group
-    //   .append("rect")
-    //     .attr("fill", "#92C5DE")
-    //     .attr("class", "legend-block")
-    //     .attr("y", legendY)
-    //     .attr("x", 0)
-    //     .attr("width", legendW)
-    //     .attr("height", legendW);
-
-    // group
-    //   .append("text")
-    //   .text(legendText[0]) // Legend label 
-    //   .attr("class", "legend-caption")
-    //   .attr("y", legendY+legendW/2.0 + 1)
-    //   .attr("x", legendW*2)
-    //   .attr('dominant-baseline','middle')
-    //   .attr('text-anchor','start');
-
-    // // Baseline indicator
-    // if (baselineData && legendText.length > 1) {
-    //   group
-    //     .append("rect")
-    //     .attr("fill", "#1f2574")
-    //     .attr("y", legendY-y.bandwidth()/2)
-    //     .attr("x", legend2X)
-    //     .attr("width", d => 4)
-    //     .attr("height", y.bandwidth()*2)
-
-    //   group
-    //     .append("text")
-    //     .text(legendText[1])
-    //     .attr("class", "legend-caption")
-    //     .attr("y", legendY+legendW/2.0)
-    //     .attr("x", legend2X+15)
-    //     .attr('dominant-baseline','middle')
-    //     .attr('text-anchor','start');
-    // }
-}
-
 function writeLine(svg, data, x, y, rootID='barid') {
   let max_y_domain = y.domain()[1];
   let max_x_domain = x.domain()[1];
   let component = this;
 
-  // console.log("Write bars data=",data);
-  // console.log("First Data",data[0]);
-  // console.log("x(0)",x(0));
-  // console.log("x(max)",x(max_x_domain-1));
-  // console.log("y(0)",y(0));
-  // console.log("y(max)",y(max_y_domain-1));
-
-      // svg.append("path")
-      // .datum(data)
-      // .attr("fill", "none")
-      // .attr("stroke", "steelblue")
-      // .attr("stroke-width", 1.5)
-      // .attr("d", d3.line()
-      //   .x(function(d) { return x(d.date) })
-      //   .y(function(d) { return y(d.value) })
-      //   )
-
-
   let groups = svg.append("g")
-    .attr('style','fill:#000000;stroke:')
-    .attr("class","fg-bars")
+    .attr("class","fg-line")
     .append('path')
-    .attr("fill","none")
-    .attr("stroke", "black")
-    .attr("stroke-width", 1.5)
     .datum(data)
       .attr("d", d3.line()
         .x(function(d,i) { return x(i) })
@@ -97,28 +25,15 @@ function writeLine(svg, data, x, y, rootID='barid') {
 }
 
 /**
- * Build data bars
- * @param {object} svg 
- * @param {object} data 
- * @param {number} x 
- * @param {number} y 
+ * Draw bars elements
+ * @param {*} svg 
+ * @param {*} data 
+ * @param {*} x 
+ * @param {*} y 
+ * @param {*} rootID 
  */
-function writeBars(svg, data, x, y, rootID='barid') {
-    let max_y_domain = y.domain()[1];
-    let max_x_domain = x.domain()[1];
-    let component = this;
-
-    // console.log("Write bars data=",data);
-    // console.log("First Data",data[0]);
-    // console.log("x(0)",x(0));
-    // console.log("x(max)",x(max_x_domain-1));
-    // console.log("y(0)",y(0));
-    // console.log("y(max)",y(max_y_domain-1));
-
-
-
+ function writeBars(svg, data, x, y, rootID='barid') {
     let groups = svg.append("g")
-      .attr('style','fill:#deeaf6')
       .attr("class","fg-bars")
       .selectAll("g")
       .data(data)
@@ -133,81 +48,118 @@ function writeBars(svg, data, x, y, rootID='barid') {
         .attr("width", 2)
         .attr("height", d => (y(0) - y(d.VALUE)))
         .attr("id", (d, i) => rootID+'-'+i);
-    
-    // // transparent hot bar (different if tooltip)
-    // groups
-    //     .append("rect")
-    //     .attr("class","hot-bar")
-    //     .attr("fill", "#00ff00")
-    //     .attr("fill-opacity",0)
-    //     .attr("y", (d, i) => y(d.CATEGORY))
-    //     .attr("x", d => x(0))
-    //     .attr("width", d => x(max_x_domain))
-    //     .attr("height", y.bandwidth())
-    //     .attr("tabindex", "0")
-    //     .attr("aria-label", (d, i) => `${this.ariaLabel(d, baselineData)}`)
-    //     .on("mouseover focus", function(event, d, i) {
-    //       d3.select(this.parentNode).select('.fg-bar')
-    //       .transition().duration(200)
-    //       .style("fill", "#003D9D");
-    //       if (tooltip) {
-    //           // set appropriate tooltip text, reveal tooltip at correct location
-    //           tooltip.html(component.getTooltip(d,baselineData))
-    //           tooltip.style("left",'20px');
-    //           // console.log("Tool top L, O, y",event.layerY, event.offsetY, event.y);
-    //           // tooltip.style("top",`${event.layerY+60}px`)
-    //           tooltip.style("top",`${event.offsetY+120}px`)
-    //           tooltip.style("visibility", "visible");
-    //       }
-    //     })
-    //     .on("mouseout blur", function(d) {
-    //       d3.select(this.parentNode).select('.fg-bar')
-    //         .transition().duration(200)
-    //         .style("fill", "#92C5DE");
-    //       if (tooltip) {
-    //         d3.select(this).transition();
-    //         tooltip.style("visibility", "hidden");
-    //       }
-    //     });
-    
+}
 
-    // // Baseline indicator
-    // if (baselineData) {
-    // groups
-    //     .append("rect")
-    //     .attr("fill", "#1f2574")
-    //     .attr("y", (d, i) => y(d.CATEGORY)-y.bandwidth()/2)
-    //     .attr("x", (d,i) => x(baselineData[i].METRIC_VALUE)-2)
-    //     .attr("width", d => 4)
-    //     .attr("height", y.bandwidth()*2);
-    // }
+/**
+ * Draw X Axis with dates
+ * @param {*} svg 
+ * @param {*} data 
+ * @param {*} x 
+ * @param {*} y 
+ * @param {*} rootID 
+ */
 
-    // // Bar Label
-    // groups
-    //   .append("text")
-    //   .attr("class", "bar-label")
-    //   .attr("y", (d, i) => y(d.CATEGORY) + (y.bandwidth() / 2))
-    //   .attr("x", d => x(max_x_domain)+12)
-    //   // .attr("width", x.bandwidth() / 4)
-    //   .text(d => this.pctFormatter.format(d.METRIC_VALUE))
-    //   // .html(d => {
-    //   //   return `<tspan dx="1.5em">${this.pctFormatter.format(d.METRIC_VALUE)}</tspan>`
-    //   // })
-    //   .attr('dominant-baseline','middle')
-    //   .attr('text-anchor','start')
 
-    // // Bar Category
-    // groups
-    //   .append("text")
-    //   .attr("class", "bar-cat")
-    //   .attr("y", (d, i) => y(d.CATEGORY)-8) // +this.getYOffset(i)
-    //   .attr("x", d => x(0))
-    //   .text(d => d.CATEGORY)
-    //   // .attr("width", x.bandwidth() / 4)
-    //   // .html(d => {
-    //   // return `<tspan dx="0em" dy="-0.5em">${d.CATEGORY}</tspan>`
-    //   // })
-    //   .attr('text-anchor','start')
+function writeDateAxis(svg, data, x, y, rootID='barid') {
+  const tick_height = 4;
+  const tick_upper_gap = 1;
+  const tick_lower_gap = 2;
+  let xgroup = svg.append("g")
+      .attr("class",'date-axis');
+
+  data.forEach((d,i) => {
+    const ymd = d.DATE.split('-');
+    const mon_idx = parseInt(ymd[1]);
+    if ((mon_idx-3) % 3 == 0) {
+      const day_idx = parseInt(ymd[2]);
+      if (day_idx == 1) {
+        const date_caption = mon_idx+'/1'; // ?? localize
+        let subg = xgroup.append("g")
+              .attr('class','x-tick');
+        subg.append('line')
+        .attr('x1', x(i))
+        .attr('y1', y(0)+tick_upper_gap)
+        .attr('x2', x(i))
+        .attr('y2',y(0)+tick_upper_gap+tick_height);
+        subg.append('text')
+         .text(date_caption)
+         .attr("x", x(i))
+         .attr("y", y(0)+tick_upper_gap+tick_height+tick_lower_gap) // +this.getYOffset(i)
+      }
+    }
+  });
+
+}
+
+function writeLeftYAxis(svg, data, x, y, y_div=20, rootID='barid') {
+  let ygroup = svg.append("g")
+      .attr("class",'left-y-axis');
+  const max_y_domain = y.domain()[1];
+  const min_x_domain = x.domain()[0];
+  const max_x_domain = x.domain()[1];
+  const tick_left_gap = 10;
+  for (let yi = 0; yi < max_y_domain; yi += y_div) {
+    let y_caption = '';
+    if (max_y_domain < 10) {
+      y_caption = this.float1Formatter.format(yi);
+    } else if (max_y_domain < 1000) {
+      y_caption = this.intFormatter.format(yi);
+    } else if (max_y_domain < 1000000) {
+      y_caption = this.intFormatter.format(yi/1000) + "K";
+    } else {
+      y_caption = this.intFormatter.format(yi/1000000) + "M";
+    }
+    let subg = ygroup.append("g")
+      .attr('class','y-tick');
+
+    subg.append('line')
+      .attr('x1', x(max_x_domain))
+      .attr('y1', y(yi))
+      .attr('x2', x(min_x_domain))
+      .attr('y2', y(yi));
+
+    subg.append('text')
+      .text(y_caption)
+      .attr("x", x(max_x_domain)-tick_left_gap)
+      .attr("y", y(yi)) // +this.getYOffset(i)
+  }
+}
+
+function writeRightYAxis(svg, data, x, y, y_div=20, rootID='barid') {
+  let ygroup = svg.append("g")
+      .attr("class",'right-y-axis');
+  const max_y_domain = y.domain()[1];
+  const min_x_domain = x.domain()[0];
+  const tick_right_gap = 10;
+  const tick_length = 4;
+
+  console.log("Drawing Right Axis  max_y_domain",max_y_domain,rootID);
+
+  for (let yi = 0; yi < max_y_domain; yi += y_div) {
+    let y_caption = '';
+    if (max_y_domain < 10) {
+      y_caption = this.float1Formatter.format(yi);
+    } else if (max_y_domain < 1000) {
+      y_caption = this.intFormatter.format(yi);
+    } else if (max_y_domain < 1000000) {
+      y_caption = this.intFormatter.format(yi/1000) + "K";
+    } else {
+      y_caption = this.intFormatter.format(yi/1000000) + "M";
+    }
+    let subg = ygroup.append("g")
+      .attr('class','y-tick');
+
+    subg.append('line')
+      .attr('x1', x(min_x_domain))
+      .attr('y1', y(yi))
+      .attr('x2', x(min_x_domain)+tick_right_gap)
+      .attr('y2', y(yi));
+
+    subg.append('text')
+      .text(y_caption)
+      .attr("x", x(min_x_domain)+tick_right_gap)
+      .attr("y", y(yi)) // +this.getYOffset(i)
+  }
 }
 
 /**
@@ -221,6 +173,8 @@ function writeBars(svg, data, x, y, rootID='barid') {
     time_series_key_bars = null,
     time_series_key_line = null,
     line_date_offset = 0,
+    left_y_div = 1000,
+    right_y_div = 0,
     root_id = "barid" } )  {
     // // this statement produces an array of strings in IE11 and an array of numbers in modern browsers
     // let categories = data.map((group) => group.CATEGORY);
@@ -259,8 +213,6 @@ function writeBars(svg, data, x, y, rootID='barid') {
    
     }
     if (time_series_key_line) {
-
-
       const LINE_OFFSET_X = line_date_offset;
       this.xline = d3
       .scaleLinear()
@@ -298,6 +250,22 @@ function writeBars(svg, data, x, y, rootID='barid') {
     if (time_series_key_line) {
       writeLine.call(this, this.svg, chartData.time_series[time_series_key_line], this.xline, this.yline, root_id);
       // line legend on right or left, depending on whether bar is provided
+    }
+    if (time_series_key_bars) {
+      writeDateAxis.call(this, this.svg, chartData.time_series[time_series_key_bars], this.xbars, this.ybars);
+    } else if (time_series_key_line) {
+      writeDateAxis.call(this, this.svg, chartData.time_series[time_series_key_line], this.xline, this.yline);
+    }
+
+    // Write Y Axis, favoring line on left, bars on right
+    if (time_series_key_line) {
+      writeLeftYAxis.call(this, this.svg, chartData.time_series[time_series_key_line], this.xline, this.yline, left_y_div, root_id);
+      if (time_series_key_bars) {
+        if (right_y_div)
+          writeRightYAxis.call(this, this.svg, chartData.time_series[time_series_key_bars], this.xbars, this.ybars, right_y_div, root_id);      }      
+    } else if (time_series_key_bars) {
+      writeLeftYAxis.call(this, this.svg, chartData.time_series[time_series_key_bars], this.xbars, this.ybars, left_y_div, root_id);
+
     }
 
     // writeLegend.call(this, this.svg, data, this.xbars, this.y, baselineData);
