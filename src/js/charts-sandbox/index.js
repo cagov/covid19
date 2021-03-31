3493968ad0183a09409b7d37153baa740a2fefd1
Jim Bumgardner <jbum@jbum.com>
2021-03-31 09:32:33 -0700

    Chart updates.

--- src/js/charts-sandbox/index.js
+++ src/js/charts-sandbox/index.js
@@ -1,5 +1,11 @@
 console.log("Loading chart sandbox");
 // import './charts/cagov-chart-vaccines-hpi-by-people/index.js'
 // import './charts/cagov-chart-vaccines-hpi-by-dose/index.js'
+import "../dashboard-v2/charts/cagov-chart-dashboard-confirmed-cases-episode-date/index.js"
+import "../dashboard-v2/charts/cagov-chart-dashboard-confirmed-deaths-death-date/index.js"
+
+import "../dashboard-v2/charts/cagov-chart-dashboard-total-tests-testing-date/index.js"
+import "../dashboard-v2/charts/cagov-chart-dashboard-positivity-rate/index.js"
+
 import "../dashboard-v2/charts/cagov-chart-dashboard-hospitalized-patients/index.js"
-// import "../dashboard-v2/charts/cagov-chart-dashboard-icu-beds/index.js"
+import "../dashboard-v2/charts/cagov-chart-dashboard-icu-beds/index.js"
