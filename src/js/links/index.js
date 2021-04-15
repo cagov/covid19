81855cca744c707e6184bd6a6b2a145bb3299363
kkoryaka <31669748+kkoryaka@users.noreply.github.com>
2021-04-15 13:02:44 -0700

    making sure that localhost urls and anchors are not adding external link icon

--- src/js/links/index.js
+++ src/js/links/index.js
@@ -14,16 +14,20 @@ for (var i=0; i < pdfLink.length; i++) {
 
 // Adding external link icon to the links with class external
 var ext = '<span class="ca-gov-icon-external-link link-icon" aria-hidden="true"></span>';
-var localurl = 'https://covid19.ca.gov';
 // Check if link is external function
 function link_is_external(link_element) {
-  return (link_element.host !== window.location.host || document.location.href.indexOf('https://covid19.ca.gov') === -1 || document.location.href.indexOf('https://staging.ca.gov') === -1 || document.location.href.indexOf('https://localhost:8000') === -1);
+ return (link_element.host !== window.location.host);
 }
+
 
 // Looping thru all link inside of the internal pages main content body
 var externalLink = document.querySelectorAll(".container .row .col-lg-10 a");
 for (var i=0; i < externalLink.length; i++) {
-  if (link_is_external(externalLink[i])) {
-  externalLink[i].innerHTML += ext; // += ccncatenates to external links
+  var anchorLink = externalLink[i].href.indexOf("#") > -1;
+  var localHost = externalLink[i].href.indexOf("localhost") > -1;
+  var localUrl = externalLink[i].href.indexOf("covid19.ca.gov") > -1;
+  if (link_is_external(externalLink[i]) && !localUrl && !localHost && !anchorLink) {
+    externalLink[i].innerHTML += ext; // += ccncatenates to external links
+
 }
 }
