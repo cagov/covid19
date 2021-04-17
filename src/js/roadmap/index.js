6dccb72517c166214f74b6dcbf16d29546f25a84
chachasikes <chachasikes@gmail.com>
2021-04-17 12:32:06 -0700

    clean up field names

--- src/js/roadmap/index.js
+++ src/js/roadmap/index.js
@@ -3,8 +3,10 @@ import templatize from './template.js';
 
 class CAGovReopening extends window.HTMLElement {
   connectedCallback () {
+
     this.json = JSON.parse(this.dataset.json);
     this.schoolsText = this.dataset.schools? JSON.parse(this.dataset.schools) : {};
+
     this.state = {};
 
     this.innerHTML = templatize(this.json);
@@ -23,9 +25,9 @@ class CAGovReopening extends window.HTMLElement {
     .then(response => response.json())
     .then(function(data) {
       this.countyStatuses = data;
-      let aList = [];
-      this.countyStatuses.forEach(c => { aList.push(c.county) })
-      this.setupAutoComp('#location-query', 'county', aList);
+      let countyAutocompleteList = [];
+      this.countyStatuses.forEach(c => { countyAutocompleteList.push(c.county) })
+      this.setupAutoComplete('#location-query', 'county', countyAutocompleteList);
     }.bind(this));
 
     window.fetch('/countyregions.json')
@@ -56,12 +58,16 @@ class CAGovReopening extends window.HTMLElement {
     .then(response => response.json())
     .then(function(data) {
       this.allActivities = data.Table1;
-      let aList = []
-      // aList.push(this.viewall);
+      let activityAutocompleteList = []
+
       data.Table1.forEach(item => {
-        aList.push(item['0'])
+        activityAutocompleteList.push(item['0'])
       })
-      this.setupAutoCompActivity('#activity-query', 'activity', aList)
+
+      console.log(activityAutocompleteList);
+
+      this.setupAutoCompleteActivity('#activity-query', 'activity', activityAutocompleteList);
+
       document.querySelector('.reopening-tableau-embed').innerHTML = `<div class='tableauPlaceholder' id='viz1598633253507' style='position: relative'><noscript><a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Pl&#47;Planforreducingcovid-19&#47;planforreducingcovid-19&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='Planforreducingcovid-19&#47;planforreducingcovid-19' /><param name='tabs' value='no' /><param name='toolbar' value='no' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Pl&#47;Planforreducingcovid-19&#47;planforreducingcovid-19&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en' /></object></div>`;
       this.tableauStuff()
     }.bind(this))
@@ -93,7 +99,10 @@ class CAGovReopening extends window.HTMLElement {
       if(document.querySelector('#activity-query').value == '') {
         this.state['activity'] = null;
       }
-      if(!this.state['activity'] && !this.state['county']) {
+      if(document.querySelector('#county-tier-query').value == '') {
+        this.state['countyTier'] = null;
+      }
+      if(!this.state['activity'] && !this.state['county'] && !this.state['countyTier']) {
         this.querySelector('.card-holder').innerHTML = '';
         document.getElementById("reopening-error").style.visibility = "visible";
       } else {
@@ -116,6 +125,20 @@ class CAGovReopening extends window.HTMLElement {
     document.getElementById("reopening-error").style.visibility = "hidden";
   }
 
+  changeCountyTierInput(value) {
+    const $countyTierQuery = document.getElementById("county-tier-query");
+    $countyTierQuery.value = value;
+    $countyTierQuery.setAttribute("aria-invalid", false);
+    this.state['countyTier'] = value;
+    if (value) {
+      document.getElementById("clearCountyTier").classList.remove('d-none');
+    } else {
+      document.getElementById("clearCountyTier").classList.add('d-none');
+    }
+    document.getElementById("county-tier-error").style.visibility = "hidden";
+    document.getElementById("reopening-error").style.visibility = "hidden";
+  }
+
   changeActivityInput(value) {
     const $activityQuery = document.getElementById("activity-query");
     $activityQuery.value = value;
@@ -130,10 +153,49 @@ class CAGovReopening extends window.HTMLElement {
     document.getElementById("reopening-error").style.visibility = "hidden";
   }
 
-  setupAutoComp(fieldSelector, fieldName, aList) {
+  setupAutoCompleteNEW(fieldSelector, fieldName, countyAutocompleteList) {
     let component = this;
     const awesompleteSettings = {
       autoFirst: true,
+      minChars: 0,
+      maxItems: 20,
+      filter: function (text, input) {
+        return Awesomplete.FILTER_CONTAINS(text, input.match(/[^,]*$/)[0]);
+      },
+      item: function (text, input) {
+        return Awesomplete.ITEM(text, input.match(/[^,]*$/)[0]);
+      },
+      replace: function (text) {
+        let before = this.input.value.match(/^.+,\s*|/)[0];
+        // @TODO clean up abbreviations
+        let autocompleteValue = before + text;
+        this.input.value = autocompleteValue;
+        component.state[fieldName] = autocompleteValue;
+        component.layoutCards();
+        document.querySelector(fieldSelector).blur();
+      },
+      sort: (a, b) => {
+        return countyAutocompleteList.indexOf(a.label) <
+          countyAutocompleteList.indexOf(b.label)
+          ? -1
+          : 1;
+      },
+      list: countyAutocompleteList,
+    };
+
+    window.makeAutocomplete = new Awesomplete(
+      fieldSelector,
+      awesompleteSettings
+    );
+  }
+
+
+  setupAutoComplete(fieldSelector, fieldName, countyAutocompleteList) {
+    let component = this;
+    const awesompleteSettings = {
+      autoFirst: true,
+      minChars: 0,
+      maxItems: 99,
       filter: function (text, input) {
         return Awesomplete.FILTER_CONTAINS(text, input.match(/[^,]*$/)[0]);
       },
@@ -146,30 +208,25 @@ class CAGovReopening extends window.HTMLElement {
         component.changeLocationInput(finalval);
         // component.layoutCards();
       },
-      list: aList
+      sort: (a, b) => {
+        return countyAutocompleteList.indexOf(a.label) <
+          countyAutocompleteList.indexOf(b.label)
+          ? -1
+          : 1;
+      },
+      list: countyAutocompleteList
     };
 
-
-    const aplete = new Awesomplete(fieldSelector, awesompleteSettings)
+    const autocompleteCounty = new Awesomplete(fieldSelector, awesompleteSettings)
+    // window.makeAutocomplete = autocompleteCounty;
   }
 
-  tableauStuff() {
-    var divElement = document.getElementById('viz1598633253507');
-    var vizElement = divElement.getElementsByTagName('object')[0];
-    if ( divElement.offsetWidth > 921 ) { vizElement.style.width='920px';vizElement.style.height='547px';} 
-    else if ( (divElement.offsetWidth > 910) && (divElement.offsetWidth < 920)) { vizElement.style.width='900px';vizElement.style.height='547px';} 
-    else if ( (divElement.offsetWidth > 700) && (divElement.offsetWidth < 899) ) { vizElement.style.width='700px';vizElement.style.height='547px';} 
-    else { vizElement.style.width='100%';vizElement.style.height='627px';}
-    var scriptElement = document.createElement('script');
-    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-    vizElement.parentNode.insertBefore(scriptElement, vizElement);
-  }
-  setupAutoCompActivity(fieldSelector, fieldName, aList) {
+  setupAutoCompleteActivity(fieldSelector, fieldName, activityAutocompleteList) {
     let component = this;
     const awesompleteSettings = {
       autoFirst: true,
       minChars: 0,
-      maxItems: 99,
+      maxItems: 20,
       sort: function(a,b) {
         if(a['0'] < b['0']) {
           return -1;
@@ -181,18 +238,19 @@ class CAGovReopening extends window.HTMLElement {
       },
       replace: function (text) {
         let before = this.input.value.match(/^.+,\s*|/)[0];
-        let finalval = before + text;
-        component.changeActivityInput(finalval);
+        let autocompleteValue = before + text;
+        component.changeActivityInput(autocompleteValue);
+        // component.state[fieldName] = autocompleteValue;
         // component.layoutCards();
         // document.querySelector(fieldSelector).blur();
       },
-      list: aList
+      list: activityAutocompleteList
     };
 
-    window.aplete2 = new Awesomplete(fieldSelector, awesompleteSettings);
+    window.autocompleteActivity = new Awesomplete(fieldSelector, awesompleteSettings);
     document.querySelector(fieldSelector).addEventListener('focus', function() {
       this.value = '';
-      window.aplete2.evaluate();
+      window.autocompleteActivity.evaluate();
     })
   }
 
@@ -296,21 +354,6 @@ class CAGovReopening extends window.HTMLElement {
           </div>`
         }
       })
-
-      // Original 2/23/2021
-      // if(this.regionsclosed && this.countyRegions && this.regionsclosed.Table1.filter(r => r.region === this.countyRegions[item.county]).length > 0) { // if this county is in a region which is closed we will show them the RSHO column values
-      //   this.cardHTML += `<div class="card-activity">
-      //     <h4>${ac["0"]}</h4>
-      //     <p>${ac["0"] === "Schools" ? schoolShenanigans(item.county) : ac["6"]}</p>
-      //     <p>${ac["0"] === "Schools" ? "" : ac["5"].indexOf('href') > -1 ? `${this.json.seeGuidanceText} ${replaceAllInMap(ac["5"])}` : ""}</p>
-      //   </div>`
-      // } else {
-      //   this.cardHTML += `<div class="card-activity">
-      //     <h4>${ac["0"]}</h4>
-      //     <p>${ac["0"] === "Schools" ? schoolShenanigans(item.county) : ac[item['Overall Status']]}</p>
-      //     <p>${ac["0"] === "Schools" ? "" : ac["5"].indexOf('href') > -1 ? `${this.json.seeGuidanceText} ${replaceAllInMap(ac["5"])}` : ""}</p>
-      //   </div>`
-      // }
     })
     // These classes are used but created with variables so the purge cannot find them, they are carefully placed here where they will be noticed
     this.cardHTML += `<div style="display:none">
@@ -318,6 +361,18 @@ class CAGovReopening extends window.HTMLElement {
     </div>`
     this.querySelector('.card-holder').innerHTML = `<div class="card-content">${this.cardHTML}</div>`;
     this.querySelector('.card-holder').classList.remove('inactive');
+  }
+
+  tableauStuff() {
+    var divElement = document.getElementById('viz1598633253507');
+    var vizElement = divElement.getElementsByTagName('object')[0];
+    if ( divElement.offsetWidth > 921 ) { vizElement.style.width='920px';vizElement.style.height='547px';} 
+    else if ( (divElement.offsetWidth > 910) && (divElement.offsetWidth < 920)) { vizElement.style.width='900px';vizElement.style.height='547px';} 
+    else if ( (divElement.offsetWidth > 700) && (divElement.offsetWidth < 899) ) { vizElement.style.width='700px';vizElement.style.height='547px';} 
+    else { vizElement.style.width='100%';vizElement.style.height='627px';}
+    var scriptElement = document.createElement('script');
+    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+    vizElement.parentNode.insertBefore(scriptElement, vizElement);
   }
 }
 
