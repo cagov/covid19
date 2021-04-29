48fc6112e82294de9ae8a1065c8ea04ab93a030a
aaronhans <aaronhans@gmail.com>
2021-04-29 13:40:37 -0700

    fix merge conflict

--- src/js/roadmap/index.js
+++ src/js/roadmap/index.js
@@ -408,10 +408,6 @@ class CAGovReopeningTierLevel extends window.HTMLElement {
       },
     });
 
-<<<<<<< HEAD
-    console.log("event", event.detail);
-=======
->>>>>>> master
     window.dispatchEvent(event);
 
     if (isError) {
