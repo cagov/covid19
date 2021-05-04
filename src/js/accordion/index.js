8a8f738c37482d607b4b19326af6d6ba293d17e8
kkoryaka <31669748+kkoryaka@users.noreply.github.com>
2021-05-04 15:27:37 -0700

    defined allbuttons variable in accodion function (ccg-2118)

--- src/js/accordion/index.js
+++ src/js/accordion/index.js
@@ -6,13 +6,19 @@ export class CaGovAccordion extends window.HTMLElement {
     this.expandButton.addEventListener('click', this.listen.bind(this));
     this.activateButton = this.querySelector('.card-header');
     this.eventType = this.dataset.eventType ? this.dataset.eventType : 'click';
+    this.addStyle(this); // Add local styles
+
     // Detect if accordion should open by default
     let expanded = this.activateButton.getAttribute('aria-expanded');
     if (expanded === "true") {
       this.triggerAccordionClick(); // Open the accordion.
       let allLinks = this.querySelectorAll(".card-container a");
+      let allbuttons = this.querySelectorAll(".card-container button");
       for (var i = 0; i < allLinks.length; i++) {
         allLinks[i].removeAttribute("tabindex"); // remove tabindex from all the links
+      }
+      for (var i = 0; i < allbuttons.length; i++) {
+        allbuttons[i].removeAttribute("tabindex"); // remove tabindex from all the buttons
       }
     }
     // making sure that all links inside of the accordion container are having tabindex -1
@@ -27,9 +33,6 @@ export class CaGovAccordion extends window.HTMLElement {
         allbuttons[i].setAttribute('tabindex', '-1');
       }
     }
-
-
-
   }
 
   listen() {
@@ -53,8 +56,6 @@ export class CaGovAccordion extends window.HTMLElement {
   }
 
   expandAccordion() {
-    // console.log("expanding accordion");
-    // this.expandTarget.setAttribute('tabindex', '-1');
     this.expandTarget.style.height = '0px';
     this.expandTarget.setAttribute('aria-hidden', 'true');
     this.querySelector('.card-header').classList.remove('accordion-alpha-open');
@@ -70,8 +71,6 @@ export class CaGovAccordion extends window.HTMLElement {
   }
 
   closeAccordion() {
-    // console.log("close accordion");
-    // this.expandTarget.removeAttribute("tabindex");
     this.expandTarget.style.height = this.cardBodyHeight + 'px';
     this.expandTarget.setAttribute('aria-hidden', 'false');
     this.querySelector('.card-header').classList.add('accordion-alpha-open');
@@ -86,10 +85,16 @@ export class CaGovAccordion extends window.HTMLElement {
       allbuttons[i].removeAttribute("tabindex"); // remove tabindex from all the buttons
     }
   }
- 
+
+
+  // Add accordion styles here
+  addStyle(element) {
+    const style = document.createElement("style");
+    style.textContent = `
+cagov-accordion .card{border-radius:0!important;border:none;margin-bottom:0;position:relative;display:flex;flex-direction:column;min-width:0;word-wrap:break-word;background-color:#fff;background-clip:border-box}cagov-accordion .card-container{display:block;overflow:hidden}cagov-accordion .card-container[aria-hidden=false]{border-bottom:1px solid #1f2574}cagov-accordion.prog-enhanced .card-container{height:0;transition:height .3s ease}cagov-accordion .collapsed{display:block;overflow:hidden;visibility:hidden}.card-header .button{color:#000}.accordion{margin-bottom:20px}.accordion-title{text-align:left;margin-bottom:0;color:#1f2574;margin-right:auto;width:90%;padding:0 .5rem 0 0!important}.accordion-title h2,.accordion-title h3,.accordion-title h4{padding:0!important;margin-top:0!important;margin-bottom:0!important;font-family:Roboto,sans-serif;font-size:1.2rem!important;font-weight:700;color:#1f2574;text-align:left!important}button.card-header{display:flex;justify-content:left;align-items:center;padding:1rem .75rem;background-clip:border-box;background-color:#fff;border:none;border-bottom:1px solid #1f2574;border-top:1px solid #1f2574;border-radius:0!important;margin-top:-1px}button.card-header:hover{background-color:#f2f5fc}button.card-header:focus{outline:2px solid #ffcf44;outline-offset:-2px}.prog-enhanced .accordion-alpha .plus-munus{width:2.125rem;height:2.125rem;border:2px solid #ff8000;border-radius:50%;padding:0;overflow:hidden;transition:all .3s ease;margin-left:1rem;color:#ff8000;align-self:flex-start}.prog-enhanced .accordion-alpha .plus-munus agov-plus .accordion-icon{display:block}.prog-enhanced .accordion-alpha .plus-munus cagov-minus .accordion-icon{display:none}.prog-enhanced .accordion-alpha-open{border-bottom-color:#fff!important}.prog-enhanced .accordion-alpha-open cagov-plus .accordion-icon{display:none}.prog-enhanced .accordion-alpha-open cagov-minus .accordion-icon{display:block!important}.dark-accordion-bg>.container.dark-accordion-first{padding-top:3rem;margin-top:3rem;padding-bottom:0;margin-bottom:0}.dark-accordion-bg>.container{padding-top:3rem;margin-top:3rem;padding-bottom:3rem;margin-bottom:3rem}.dark-accordion-sibling>.container{padding-top:0;padding-bottom:0;margin-top:0;margin-bottom:0}.dark-accordion-sibling+.dark-accordion-bg>.container{padding-top:0;margin-top:0;padding-bottom:3rem;margin-bottom:3rem}.dark-accordion-bg .card{background-color:#1f2574}.dark-accordion-bg .card .accordion-alpha{background-color:#1f2574;border-bottom:1px solid #4c5190;border-top:1px solid #4c5190}.dark-accordion-bg .card .accordion-alpha.accordion-alpha-open{border-bottom-color:#1f2574!important}.dark-accordion-bg .card .accordion-alpha:focus,.dark-accordion-bg .card .accordion-alpha:hover{background-color:#003d9d}.dark-accordion-bg .card .accordion-alpha .accordion-title{color:#fff}.dark-accordion-bg .card .accordion-alpha .accordion-title h2,.dark-accordion-bg .card .accordion-alpha .accordion-title h3,.dark-accordion-bg .card .accordion-alpha .accordion-title h4{color:#fff}.dark-accordion-bg .card .card-container{color:#fff}.dark-accordion-bg .card .card-container[aria-hidden=false]{border-bottom:1px solid #4c5190}.dark-accordion-bg .card .card-container h2,.dark-accordion-bg .card .card-container h3,.dark-accordion-bg .card .card-container h4,.dark-accordion-bg .card .card-container h5{color:#fff}.bg-darkblue .dark-accordion-bg>.container.dark-accordion-first{padding-top:1rem;margin-top:0;padding-bottom:0;margin-bottom:0}.fill-orange{fill:#ff8000!important}.card-body{padding:.55rem .45rem .55rem .75rem;color:#000;line-height:1.5}.card-body p{font-family:Roboto,sans-serif;font-weight:300}
+    `;
+    element.appendChild(style);
+  }
 
 }
-
-
-
 window.customElements.define('cagov-accordion', CaGovAccordion);
