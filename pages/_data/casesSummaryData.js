8b0aaa94a641e0dae9e9e500c1533daf4c7e3a0d
Jim Bumgardner <jbum@jbum.com>
2021-09-10 10:45:36 -0700

    Using precomputed fields for daily averages.

--- pages/_data/casesSummaryData.js
+++ pages/_data/casesSummaryData.js
@@ -13,32 +13,35 @@ module.exports = function() {
     fetch(dataDomain+'confirmed-cases/california.json')
     .then(res => res.json())
     .then(json => {
-        const data = json;          
-        let sumCasesCount = 0;
-        const pending_date = json.data.latest.CONFIRMED_CASES.EPISODE_UNCERTAINTY_PERIOD;
-        const caseList = json.data.time_series.CONFIRMED_CASES_EPISODE_DATE.VALUES;
-        let parse_state = 0;
-        let summed_days = 0;
-        for (let i = 0; i < caseList.length; ++i) {
-            if (parse_state == 0) {
-                if (caseList[i].DATE == pending_date) {
-                    parse_state = 1;
-                }
-            } else {
-                sumCasesCount += caseList[i].VALUE;
-                summed_days += 1;
-            }
-            if (summed_days == 7) {
-                break;
-            }
-        }
-        // console.log("SUMMED CASES", sumCasesCount, summed_days);
-        let output = 
-            {
-                CASES_DAILY_AVERAGE: sumCasesCount / summed_days
-            }
-        ;
-        resolve(output);
+        // const data = json;          
+        // let sumCasesCount = 0;
+        // const pending_date = json.data.latest.CONFIRMED_CASES.EPISODE_UNCERTAINTY_PERIOD;
+        // const caseList = json.data.time_series.CONFIRMED_CASES_EPISODE_DATE.VALUES;
+        // let parse_state = 0;
+        // let summed_days = 0;
+        // for (let i = 0; i < caseList.length; ++i) {
+        //     if (parse_state == 0) {
+        //         if (caseList[i].DATE == pending_date) {
+        //             parse_state = 1;
+        //         }
+        //     } else {
+        //         sumCasesCount += caseList[i].VALUE;
+        //         summed_days += 1;
+        //     }
+        //     if (summed_days == 7) {
+        //         break;
+        //     }
+        // }
+        // // console.log("SUMMED CASES", sumCasesCount, summed_days);
+        // let output = 
+        //     {
+        //         CASES_DAILY_AVERAGE: sumCasesCount / summed_days
+        //     }
+        // ;
+
+        // console.log("CASE COMPARISON (dyn/pre): ",sumCasesCount / summed_days, json.data.latest.CONFIRMED_CASES.CASES_DAILY_AVERAGE);
+        // resolve(output);
+        resolve(json);
     });
   });
 };
--- pages/_data/deathsSummaryData.js
+++ pages/_data/deathsSummaryData.js
@@ -13,32 +13,34 @@ module.exports = function() {
     fetch(dataDomain+'confirmed-deaths/california.json')
     .then(res => res.json())
     .then(json => {
-        const data = json;          
-        let sumDeathsCount = 0;
-        const pending_date = json.data.latest.CONFIRMED_DEATHS.DEATH_UNCERTAINTY_PERIOD;
-        const deathsList = json.data.time_series.CONFIRMED_DEATHS_DEATH_DATE.VALUES;
-        let parse_state = 0;
-        let summed_days = 0;
-        for (let i = 0; i < deathsList.length; ++i) {
-            if (parse_state == 0) {
-                if (deathsList[i].DATE == pending_date) {
-                    parse_state = 1;
-                }
-            } else {
-                sumDeathsCount += deathsList[i].VALUE;
-                summed_days += 1;
-            }
-            if (summed_days == 7) {
-                break;
-            }
-        }
-        // console.log("SUMMED DEATHS", sumDeathsCount, summed_days);
-        let output = 
-            {
-                DEATHS_DAILY_AVERAGE: sumDeathsCount / summed_days
-            }
-        ;
-        resolve(output);
+        // const data = json;          
+        // let sumDeathsCount = 0;
+        // const pending_date = json.data.latest.CONFIRMED_DEATHS.DEATH_UNCERTAINTY_PERIOD;
+        // const deathsList = json.data.time_series.CONFIRMED_DEATHS_DEATH_DATE.VALUES;
+        // let parse_state = 0;
+        // let summed_days = 0;
+        // for (let i = 0; i < deathsList.length; ++i) {
+        //     if (parse_state == 0) {
+        //         if (deathsList[i].DATE == pending_date) {
+        //             parse_state = 1;
+        //         }
+        //     } else {
+        //         sumDeathsCount += deathsList[i].VALUE;
+        //         summed_days += 1;
+        //     }
+        //     if (summed_days == 7) {
+        //         break;
+        //     }
+        // }
+        // // console.log("SUMMED DEATHS", sumDeathsCount, summed_days);
+        // let output = 
+        //     {
+        //         DEATHS_DAILY_AVERAGE: sumDeathsCount / summed_days
+        //     }
+        // ;
+        // console.log("DEATH COMPARISON (dyn/pre): ",sumDeathsCount / summed_days, json.data.latest.CONFIRMED_DEATHS.DEATHS_DAILY_AVERAGE);
+        // resolve(output);
+        resolve(json);
     });
   });
 };
