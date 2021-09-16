2cc6f0c0080d3b4ef3bda25dde155b510d084560
Zakiya <zakiya.khabir@digital.ca.gov>
2021-09-15 18:32:27 -0700

    [5278] - Design labels so they don't overlap.

--- src/js/equity-dash/charts/healthy-places-index/tooltip.js
+++ src/js/equity-dash/charts/healthy-places-index/tooltip.js
@@ -8,48 +8,67 @@ export default class Tooltip {
     // this.legend = legend; // no longer using this param
     this.node = document.createElementNS("http://www.w3.org/2000/svg","g")
     this.node.setAttribute("pointer-events","none")
-    this.node.setAttribute("text-anchor","left")
     this.node.innerHTML = 
+    
+    // Pale, vertical line.
     (needs_anno? 
        `<rect x="0" width=0.25 y=${yRange[1]} height=${(yRange[0] - yRange[1])} fill="#003D9D" stroke=none opacity=0.5></rect>
         <rect x="-5.5" width="10" y="89.5" height="3.5" fill="white" opacity=0.5></rect>
         <text class="date" y="62", x="0"></text>`
       : '') + 
-    `<g class="tooltip-container">
-       <!-- <rect x="2" width="28" y="-5.5" height="3.5" fill="white" opacity=0.5></rect> -->
-       <text class="label1" y="-1", x="2"></text>
-       <circle r="3" dx="3" fill="#0F368E"></circle>
-     </g>`;
-  } // <!-- <text class="label2" y="3", x="2"></text> -->
+
+    // Tip.
+    `<g class="tip">
+      <rect height="38" y="-25"></rect> 
+      <text class="label1" x="0"></text>
+      <circle r="3" dx="3" fill="#0F368E"></circle>
+    </g>
+    `;
+  } 
   show(d,x,y, lineIsOnTop, label) {
     let yVal = d.METRIC_VALUE;
     // console.log("Showing: ",d);
-    this.node.removeAttribute("display");
     let tranX = x(parseSnowflakeDate(d.DATE));
+    // console.log("Tran X: ",tranX);
     let tranY = y(yVal) + 4;
     let xRange = x.range();
+
+    // 70 is halfway point of svg...
     let midPoint = (xRange[0]+xRange[1])/2;
     
     // The dynamic number that shows in the tip.
     let tipNumber = (yVal * 100).toFixed(1);
     // Change the y position of the tool tip based on its relative value.
-    let adjustY =  (lineIsOnTop) ? -30 : 30;
+    let adjustY =  (lineIsOnTop) ? 45 : -45;
     
-    // console.log("Tran X: ",tranX);
-    // 70 is halfway point of svg...
-    if (tranX > midPoint) {
-      this.node.querySelector('text.label1').setAttribute('text-anchor','end')
-      this.node.querySelector('text.label1').setAttribute('x','-4')
-    } else {
-      this.node.querySelector('text.label1').setAttribute('text-anchor','start')
-      this.node.querySelector('text.label1').setAttribute('x','4')
-    }
-  
-    this.node.setAttribute("transform", `translate(${tranX},${adjustY})`);
-    this.node.setAttribute("aria-label", tipNumber);
-    this.node.querySelector(".tooltip-container").setAttribute("transform", `translate(0,${tranY - 4 })`);
-    this.node.querySelector(".tooltip-container").setAttribute("style", "visibility: visible;");
-    this.node.querySelector('text.label1').innerHTML = `<tspan font-weight="bold">${tipNumber}%</tspan><tspan>${label.toLowerCase()}</tspan>`;
+    // @todo - Get dynamically from text width.
+    const rectangleWidth = 240;
+
+    // .tip
+    const tip = this.node;
+    tip.removeAttribute("display");
+    tip.setAttribute('text-anchor',(tranX > midPoint) ? 'end' :'start')
+    tip.setAttribute("transform", `translate(${tranX}, 0)`);
+    tip.setAttribute("aria-label", tipNumber);
+
+    // .tip text
+    const text = this.node.querySelector('text.label1');
+    text.setAttribute('x', (tranX > midPoint) ? 0  : 10);
+    text.innerHTML = `<tspan font-weight="bold">${tipNumber}%</tspan>&nbsp;<tspan>${label.toLowerCase()}</tspan>`;
+    // text.setAttribute("transform", `translate(0,${adjustY})`);
+    // text.setAttribute("transform", `translate(0,${tranY - 4 })`);
+    text.setAttribute("transform", `translate(0,${tranY + adjustY })`);
+
+    // .tip rect
+    const rectangle = this.node.querySelector(".tip rect");
+    rectangle.setAttribute("width", rectangleWidth);
+    rectangle.setAttribute('x',(tranX > midPoint) ? -(rectangleWidth) + 10 : -10);
+    // rectangle.setAttribute("transform", `translate(0,${adjustY})`);
+    rectangle.setAttribute("transform", `translate(0,${tranY + adjustY})`);
+
+    // .tip circle
+    const circle = this.node.querySelector(".tip circle");
+    circle.setAttribute("transform", `translate(0,${tranY - 4})`);
 
     /***** 
     // date alignment debugging
