64f2a471b56fa7153e1c5850ad35195d87747855
Jim Bumgardner <jbum@jbum.com>
2021-10-19 16:03:23 -0700

    Variant chart, first pass.

---
+++ src/js/chart-renderer/charts/cagov-chart-dashboard-variant-chart/variantchart-template.js
@@ -0,0 +1,13 @@
+import css from "./variantchart.scss";
+/**
+ * Generic template for mixed line/bar charts on State Dashboard
+ * 
+ */
+export default function template(chartOptions, {
+  post_chartTitle = "chart title",   /* Unused, but left in for future use */
+}) {
+
+  return /*html*/ `
+            <div class="svg-holder"></div>
+    `;
+}
--- src/js/chart-renderer/index.js
+++ src/js/chart-renderer/index.js
@@ -2,3 +2,6 @@
 import "./charts/cagov-chart-dashboard-sparkline/index.js"
 // postvax charts
 import "./charts/cagov-chart-dashboard-postvax-chart/index.js"
+// variant charts
+import "./charts/cagov-chart-dashboard-variant-chart/index.js"
+
