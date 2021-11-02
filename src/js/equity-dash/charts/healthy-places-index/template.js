77d492ef29e415b71acb5b1e3e1a3d55c3daa59e
zakiya <zakiya@users.noreply.github.com>
2021-11-02 11:17:22 -0700

    #5555 Equity page line chart width.

--- src/js/equity-dash/charts/healthy-places-index/template.js
+++ src/js/equity-dash/charts/healthy-places-index/template.js
@@ -1,16 +1,10 @@
 import css from './index.scss';
 
 export default function template(translationsObj) {
-  return /*html*/`<div class="py-2 d-none">
-    <div class="container">
-      <div class="col-lg-12 bg-white py-4">
-        <div class="row">
-          <div class="col-lg-12 col-md-12 col-sm-12 mx-auto px-3">
-            <div class="svg-holder">
-            </div>
-          </div>
-        </div>
-      </div><!--END col-12-->
-    </div><!--END CONTAINER-->
+  return /*html*/`<div class="py-2">
+    <div class="bg-white py-4">
+      <div class="svg-holder">
+      </div>
+    </div>
   </div>`;
 }
