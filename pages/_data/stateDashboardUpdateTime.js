7d85b317745b0cd9b9c2bcb40be38b2efeed37fc
Jim Bumgardner <jbum@jbum.com>
2021-11-18 10:46:07 -0800

    Using supplied update times instead of canned 10AM for dashboards.

---
+++ pages/_data/stateDashboardUpdateTime.js
@@ -0,0 +1,18 @@
+// stateDashboardUpdateTime
+//
+// Retrieves json file for use in state dashboard
+//
+const fetch = require('node-fetch')
+
+module.exports = function() {
+  let dataDomain = 'https://raw.githubusercontent.com/cagov/covid-static-data/main/';
+    return new Promise((resolve, reject) => {
+    fetch(dataDomain+'status/last_dashboard_update.json')
+    .then(res => res.json())
+    .then(json => {
+        resolve(json);
+    });
+  });
+};
+
+
