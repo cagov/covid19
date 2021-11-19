ac0cbfd9858e17546ce8f88523c51758d6a71397
Jim Bumgardner <jbum@jbum.com>
2021-11-19 13:26:48 -0800

    Variant chart mockup with static data.

--- src/js/chart-renderer/index.js
+++ src/js/chart-renderer/index.js
@@ -2,3 +2,5 @@
 import "./charts/cagov-chart-dashboard-sparkline/index.js"
 // postvax charts
 import "./charts/cagov-chart-dashboard-postvax-chart/index.js"
+// variant charts
+import "./charts/cagov-chart-dashboard-variant-chart/index.js"
