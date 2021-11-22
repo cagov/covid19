3fcff565bde331aa5484c6c521ea5825087f2385
Jim Bumgardner <jbum@jbum.com>
2021-11-22 11:22:35 -0800

    Moved variants chart to (new) variants page.

--- src/js/chart-renderer/index.js
+++ src/js/chart-renderer/index.js
@@ -2,5 +2,3 @@
 import "./charts/cagov-chart-dashboard-sparkline/index.js"
 // postvax charts
 import "./charts/cagov-chart-dashboard-postvax-chart/index.js"
-// variant charts
-import "./charts/cagov-chart-dashboard-variant-chart/index.js"
---
+++ src/js/variants/rollup.config.js
@@ -0,0 +1,37 @@
+import resolve from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
+import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss';
+
+
+const defaultConfig = {
+  chartsStateDashTablesLoc: 'https://data.covid19.ca.gov/data/dashboard/',
+}
+const stagingConfig =  {
+  chartsStateDashTablesLoc: 'https://raw.githubusercontent.com/cagov/covid-static-data/CovidStateDashboardTables_Staging/data/dashboard/',
+}
+
+const devOutputPath = 'docs/js/variants.js';
+const prodOutputPath = 'pages/_buildoutput/variants.js';
+const outputPath = (process.env.NODE_ENV === 'development') ? devOutputPath : prodOutputPath;
+const jsConfig = (process.env.NODE_ENV === 'staging' || process.env.NODE_ENV == "development") ? stagingConfig : defaultConfig;
+
+export default {
+  input: 'src/js/variants/index.js',
+  output: {
+    intro: 'const config = '+JSON.stringify(jsConfig),
+    file: outputPath,
+    format: 'esm'
+  },
+  plugins: [resolve(), 
+            postcss({
+              extract: false,
+              modules: false,
+              use: ['sass'],
+            }),
+            terser(),
+            json()
+          ]
+};
+
+
