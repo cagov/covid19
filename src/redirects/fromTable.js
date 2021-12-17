3de9c8cf447d6a2487dddcb5b51c1c91af2a4926
Carter Medlin <carterm@users.noreply.github.com>
2021-12-16 16:36:58 -0800

    Got working code for loading redirects

--- src/redirects/fromTable.js
+++ src/redirects/fromTable.js
@@ -1,32 +1,44 @@
-//const fs = require('fs')
-//const AWS = require('aws-sdk')
-//const s3 = new AWS.S3();
+const AWS = require('aws-sdk')
 
-const Bucket = 'staging.cannabis.ca.gov';
+const s3 = new AWS.S3();
 
+const Bucket = 'covid19.ca.gov';
 const redirectJson = require('../../pages/wordpress-posts/page-redirect-table.json');
 
+if (!s3.config.credentials) {
+    //Use this for local dev with a awe_config.json file
+    s3.config.loadFromPath('../../aws_config.json');
+    //AWS config file in root should look like this...
+    //
+    //{
+    //    "credentials": {
+    //        "accessKeyId": "<YOUR_ACCESS_KEY_ID>",
+    //        "secretAccessKey": "<YOUR_SECRET_ACCESS_KEY>"
+    //    },
+    //    "region": "us-west-1"
+    //}
+}
+
+//Remove leading slashes, and anything that ends in slash will get a /index.html added
 
 const s3Input = redirectJson.Table1.map(redirect => ({
-    Key: (redirect.original_url.indexOf('/') === 0) ? redirect.original_url.substring(1) : redirect.original_url,
-    WebsiteRedirectLocation: redirect.replacement_url,
-    StatusCode: redirect.replacement_url ? 301 : 410
+    Key: redirect.original_url.replace(/^\//, '').replace(/\/$/, '/index.html'),
+    WebsiteRedirectLocation: redirect.replacement_url || '/410'
 }));
 
-console.log(JSON.stringify(s3Input, null, 2));
 
-if (false) {
-    s3Input.forEach(redirect => {
-        console.log('Uploading ', redirect)
-        s3.putObject({
-            Body: '',
-            Bucket,
-            Key: redirect.Key,
-            WebsiteRedirectLocation: redirect.WebsiteRedirectLocation,
-            StatusCode: redirect.StatusCode
-        }, (err, data) => {
-            if (err) return console.log(err)
-            console.log(data)
-        })
+//console.log(JSON.stringify(s3Input, null, 2));
+
+//https://docs.aws.amazon.com/sdkfornet1/latest/apidocs/html/T_Amazon_S3_Model_PutObjectRequest.htm
+s3Input.forEach(redirect => {
+    //console.log('Uploading ', redirect)
+    s3.putObject({
+        Bucket,
+        Key: redirect.Key,
+        Body: '', //Creates an empty file placeholder
+        WebsiteRedirectLocation: redirect.WebsiteRedirectLocation
+    }, (err, data) => {
+        if (err) return console.log(err)
+        console.log(data)
     })
-}
+})
