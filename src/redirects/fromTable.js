a1195dd6c7eaa0d126fd25ef03faa69f090d89d9
Carter Medlin <carterm@users.noreply.github.com>
2021-12-16 17:20:09 -0800

    added sample code for changing bucket config

--- src/redirects/fromTable.js
+++ src/redirects/fromTable.js
@@ -26,9 +26,41 @@ const s3Input = redirectJson.Table1.map(redirect => ({
     WebsiteRedirectLocation: redirect.replacement_url || '/410'
 }));
 
+/*
+//Use this code if you want to dynamically change the website redirect rules
+//https://docs.aws.amazon.com/sdkfornet1/latest/apidocs/html/T_Amazon_S3_Model_PutBucketWebsiteRequest.htm
+s3.putBucketWebsite({
+    Bucket,
+    WebsiteConfiguration: {
+        IndexDocument: {
+            Suffix: "index.html"
+        }
+        ,
+        ErrorDocument: {
+            Key: "404/index.html"
+        },
+        RoutingRules: [{
+            Condition: {
+                KeyPrefixEquals: "nothing/"
+            },
+            Redirect: {
+                ReplaceKeyPrefixWith: "something/"
+            }
+        }]
+    }
+}, (err, data) => {
+    if (err) return console.log(err)
+    console.log(data)
+});
+*/
+
 
 //console.log(JSON.stringify(s3Input, null, 2));
 
+//Explanation of setting object redirects...
+//https://docs.aws.amazon.com/AmazonS3/latest/userguide/how-to-page-redirect.html#advanced-conditional-redirects
+
+//API Method docs...
 //https://docs.aws.amazon.com/sdkfornet1/latest/apidocs/html/T_Amazon_S3_Model_PutObjectRequest.htm
 s3Input.forEach(redirect => {
     //console.log('Uploading ', redirect)
