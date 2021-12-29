f067a6ee8c3dc5446ee8ce2b15e39820db827f3a
Jim Bumgardner <jbum@jbum.com>
2021-12-29 09:45:01 -0800

    Added accurate date reference files. Using accurate date for equity page.

---
+++ pages/_data/stateDashboardVaxUpdateTime.js
@@ -0,0 +1,16 @@
+// stateDashboardVaxUpdateTime
+//
+// Retrieves json file for use in state dashboard
+//
+const fetch = require('node-fetch')
+
+module.exports = function() {
+  let dataDomain = 'https://raw.githubusercontent.com/cagov/covid-static-data/main/';
+    return new Promise((resolve, reject) => {
+    fetch(dataDomain+'data/status/last_vax_update.json')
+    .then(res => res.json())
+    .then(json => {
+        resolve(json);
+    });
+  });
+};
