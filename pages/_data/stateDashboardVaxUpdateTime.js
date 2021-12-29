e3779e3b466cebe065b2c28a094f2bef05bc3ee5
Jim Bumgardner <jbum@jbum.com>
2021-12-29 09:48:41 -0800

    Added error handlng.

--- pages/_data/stateDashboardVaxUpdateTime.js
+++ pages/_data/stateDashboardVaxUpdateTime.js
@@ -11,6 +11,9 @@ module.exports = function() {
     .then(res => res.json())
     .then(json => {
         resolve(json);
+      }).catch((error) => {
+        reject({"error": error})
+      });
     });
-  });
-};
+  };
+  
