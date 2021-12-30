d80d5056789b0f12fde9c1a5c01a01c476cb5567
Jim Bumgardner <jbum@jbum.com>
2021-12-30 13:39:32 -0800

    Normalized variants chart.

--- src/js/variants/charts/cagov-chart-dashboard-variant-chart/index.js
+++ src/js/variants/charts/cagov-chart-dashboard-variant-chart/index.js
@@ -16,7 +16,6 @@ class CAGovDashboardVariantChart extends window.HTMLElement {
     this.translationsObj = getTranslations(this);
     this.chartConfigFilter = this.dataset.chartConfigFilter;
     this.chartConfigKey = this.dataset.chartConfigKey;
-    this.chartConfigTimerange = this.dataset.chartConfigTimerange;
     this.chartOptions = chartConfig[this.chartConfigKey][this.chartConfigFilter];
     this.dataLoaded = false;
 
@@ -95,12 +94,10 @@ class CAGovDashboardVariantChart extends window.HTMLElement {
   }
 
   renderComponent() {
-    this.chartData = this.cropData(this.chartConfigTimerange, this.uncroppedChartData);
-
     // collect dates here...
     const chart_publish_date = this.chartmeta.PUBLISHED_DATE;
     const chart_report_date = this.chartmeta.REPORT_DATE; // unused
-    const sampleSeries = this.chartData.time_series.Alpha_Cases.VALUES;
+    const sampleSeries = this.chartdata.time_series.Alpha_Cases.VALUES;
     const chart_last_date = sampleSeries[sampleSeries.length-1].DATE;
     const repDict = {
       CHART_PUBLISH_DATE: reformatReadableDate(chart_publish_date),
@@ -112,15 +109,39 @@ class CAGovDashboardVariantChart extends window.HTMLElement {
 
     this.innerHTML = template.call(this, this.chartOptions, this.translationsObj);
 
-    this.setupSelectFilters();
-
     let line_series_array = [];
 
+    // console.log("KEYS");
+    // console.log(Object.keys(this.chartdata.time_series));
+
+
     this.chartlabels.forEach((label, i) => {
-        let tseries_name = label + "_Percentage,-7-day average";
-        line_series_array.push(this.chartData.time_series[tseries_name].VALUES);
+        let tseries_name = label + this.chartOptions.tseries_suffix;
+        line_series_array.push(this.chartdata.time_series[tseries_name].VALUES);
     });
+    if (this.chartOptions.normalize) {
+      console.log("Normalizing");
+      line_series_array = JSON.parse(JSON.stringify(line_series_array)); // clone it
+      const nbrRecords = line_series_array[0].length;
+      const nbrLines = line_series_array.length;
+      for (let i = 0; i < nbrRecords; ++i) { 
+        let sum = 0;
+        for (let j = 0; j < nbrLines; ++j) {
+          sum += line_series_array[j][i].VALUE;
+        }
+        if (sum > 0) {
+          let norm_scale = 100.0/sum;
+          for (let j = 0; j < nbrLines; ++j) {
+            sum += line_series_array[j][i].VALUE *= norm_scale;
+          }
+        }
+      }
+
+    }
     this.line_series_array = line_series_array;
+
+
+    // console.log("Rendering variants chart",this.translationsObj, this.line_series_array);
 
     let renderOptions = {
                           'chart_options':this.chartOptions,
@@ -135,88 +156,29 @@ class CAGovDashboardVariantChart extends window.HTMLElement {
                           'render_date': getSnowflakeStyleDate(0),
                           'chart_options': this.chartOptions,
                           'series_labels': this.chartlabels,
-                          'series_colors': this.chartlabels.length == 8? this.chartOptions.series_colors8 : this.chartOptions.series_colors9,
+                          'series_colors': this.chartOptions.series_colors,
                         };
       // console.log("RENDERING CHART",this.chartConfigFilter, this.chartConfigKey);
       // console.log("SERIES COLORS LENGTH", this.chartlabels.length);
       renderChart.call(this, renderOptions);
   }
 
-  chartFilterSelectsHandler(selectFilters, e) {
-    selectFilters.forEach((select) => {
-      switch (select.dataset.type) {
-        case 'time':
-          this.chartConfigTimerange = select.value;
-          break;
-        case 'filter':
-          this.chartConfigFilter = select.value;
-          break;
-        default:
-      }
-    });
-    this.renderComponent(this.regionName);
-  }
-
-  cropData(timerangeKey, uncroppedChartData) {
-    // console.log("Would crop data",timerangeKey, uncroppedChartData);
-
-    const unitSizeDict = {'months':31,'month':31,'days':1,'day':1};
-
-    let daysToKeep = -1;
-    const tokens = timerangeKey.split('-');
-    if (tokens[0] in unitSizeDict) {
-      daysToKeep = unitSizeDict[tokens[0]] * parseInt(tokens[1]);
-    }
-    let chartData = JSON.parse(JSON.stringify(uncroppedChartData)); // deep copy
-    let keys = Object.keys(chartData.time_series);
-    if (daysToKeep > 0) {
-      keys.forEach( (key) => {
-        const chartSeries = chartData.time_series[key];
-        chartSeries.VALUES = chartSeries.VALUES.splice(chartSeries.VALUES.length-daysToKeep);
-        // const lastValue = chartSeries.VALUES[chartSeries.VALUES.length-1];
-        // chartSeries.DATE_RANGE.MINIMUM = lastValue.DATE;
-      });
-    }
-    // console.log("Done Cropping", chartData);
-    return chartData; 
-
-
-
-    return chartData;
-  }  
-
-  // Add event listener to select filters.
-  setupSelectFilters() {
-    const selectFilters = document.querySelectorAll(`cagov-chart-filter-select.js-filter-${this.chartConfigKey} select`);
-
-    selectFilters.forEach((selectFitler) => {
-      selectFitler.addEventListener(
-        'change',
-        this.chartFilterSelectsHandler.bind(this, selectFilters),
-        false,
-      );
-    });
-  }
-
-
-
   retrieveData(url) {
-    // console.log("FETCHING",url);
+    console.log("FETCHING",url);
     window
       .fetch(url)
       .then((response) => response.json())
       .then(
         function (vchart_vdata) {
-          this.chartData = vchart_vdata.data;
-          this.uncroppedChartData = vchart_vdata.data;
+          this.chartdata = vchart_vdata.data;
           this.chartmeta = vchart_vdata.meta;
           this.chartlabels = this.chartOptions.chart_labels; // vchart_vdata.meta.VARIANTS;
           this.dataLoaded = true;
     
           // Splice for dates
-          const tsKeys = Object.keys(this.chartData.time_series);
+          const tsKeys = Object.keys(this.chartdata.time_series);
           tsKeys.forEach((tseriesnom) => {
-            let tseries = this.chartData.time_series[tseriesnom].VALUES;
+            let tseries = this.chartdata.time_series[tseriesnom].VALUES;
             let nbr_to_chop = 0;
             tseries.forEach((rec, i) => {
               if (rec.DATE == this.chartOptions.starting_date) {
