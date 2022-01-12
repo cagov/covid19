1729b358cffcf20ba2fa9e56fdb51aedd6f1d2ac
Jim Bumgardner <jbum@jbum.com>
2022-01-12 14:44:57 -0800

    Removed postvax chart from chart-renderer.

--- src/js/chart-renderer/index.js
+++ src/js/chart-renderer/index.js
@@ -1,4 +1,4 @@
 // sparklines
 import "./charts/cagov-chart-dashboard-sparkline/index.js"
 // postvax charts
-import "./charts/cagov-chart-dashboard-postvax-chart/index.js"
+// import "./charts/cagov-chart-dashboard-postvax-chart/index.js"
