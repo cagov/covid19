38fb176aac2ec811132a03ea48debaa173100b1b
Jim Bumgardner <jbum@jbum.com>
2022-01-19 13:48:07 -0800

    Modified pending label logic, and added 4 days lag.

--- src/js/variants/charts/cagov-chart-dashboard-variant-chart/variantchart-render.js
+++ src/js/variants/charts/cagov-chart-dashboard-variant-chart/variantchart-render.js
@@ -269,11 +269,14 @@ function writePendingBlock(svg, x, y,
     root_id='barid',
   } ) {
 
+    const min_x_domain = x.domain()[0];
     const max_x_domain = x.domain()[1];
     const min_y_domain = y.domain()[0];
     const max_y_domain = y.domain()[1];
     const left_edge = x(max_x_domain-padDays + 0.5 - pending_days);
     const right_edge = x(max_x_domain-padDays);
+    const mid_edge = (left_edge + right_edge) / 2;
+    const right_chart_edge = x(max_x_domain);
 
     let xgroup = svg.append("g")
       .attr("class",'pending-block');
@@ -284,11 +287,12 @@ function writePendingBlock(svg, x, y,
       .attr("y",y(max_y_domain))
       .attr("width",right_edge - left_edge)
       .attr("height",y(min_y_domain)-y(max_y_domain));
+    
     xgroup.append('text')
-      // .attr('style','font-family:sans-serif; fill:black; font-weight:300; font-size: 0.8rem; text-anchor: end; dominant-baseline:auto;')
-      .text(pending_legend)
-      .attr("x",x(max_x_domain))
-      .attr("y",y(max_y_domain)-4);
+        // .attr('style','font-family:sans-serif; fill:black; font-weight:300; font-size: 0.8rem; text-anchor: end; dominant-baseline:auto;')
+        .text(pending_legend)
+        .attr("x",mid_edge+22 > right_chart_edge? right_chart_edge : mid_edge+22)
+        .attr("y",y(max_y_domain)-4);
 }
 
 // Convert 
@@ -432,8 +436,8 @@ function getAxisDiv(ascale,{hint='num'}) {
     // Determine additional days to add...
     const lastDateSnowFlake = line_series_array[0][line_series_array[0].length-1].DATE;
     const lastDateJ = parseSnowflakeDate(lastDateSnowFlake);
-    // pad to 21 days
-    let padDays = Math.max(0, 21 - lastDateJ.getDate());
+    // pad to 7 days
+    let padDays = Math.max(0, 14 - lastDateJ.getDate());
 
     this.xline = d3
     .scaleLinear()
