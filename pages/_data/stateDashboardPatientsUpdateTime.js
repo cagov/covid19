7abf8f591889977ca58a453ef074b96efc5fd466
James Logan <jamesstanleylogan@gmail.com>
2023-06-29 19:26:54 -0700

    hosp updates

---
+++ pages/_data/stateDashboardPatientsUpdateTime.js
@@ -0,0 +1,20 @@
+// stateDashboardPatientsUpdateTime
+//
+// Retrieves json file for use in state dashboard
+//
+const fetch = require('node-fetch')
+
+module.exports = function() {
+  let dataDomain = 'https://data.covid19.ca.gov/';
+    return new Promise((resolve, reject) => {
+    fetch(dataDomain+'data/status/last_patients_update.json')
+    .then(res => res.json())
+    .then(json => {
+        resolve(json);
+     }).catch((error) => {
+       reject({"error": error})
+     });
+   });
+ };
+  
+
