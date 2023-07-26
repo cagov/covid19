67b64ebca86be487eebdafc7e108c52fb02cff5d
James Logan <jamesstanleylogan@gmail.com>
2023-07-26 15:04:35 -0700

    add dev sparklines (#7570)

--- src/js/main/rollup.config.js
+++ src/js/main/rollup.config.js
@@ -5,9 +5,19 @@ const devOutputPath = 'docs/js/main.js';
 const prodOutputPath = 'pages/_buildoutput/main.js';
 const outputPath = (process.env.NODE_ENV === 'development') ? devOutputPath : prodOutputPath;
 
+const defaultConfig = {
+    svg_path: 'https://files.covid19.ca.gov/img/generated/sparklines/'
+}
+const stagingConfig = {
+    svg_path: 'https://raw.githubusercontent.com/cagov/covid-static/main/img/generated/sparklines_staging/'
+}
+
+const jsConfig = (process.env.NODE_ENV === 'staging' || process.env.NODE_ENV == "development") ? stagingConfig : defaultConfig;
+
 export default {
   input: 'src/js/main/index.js',
   output: {
+    intro: 'const config = '+JSON.stringify(jsConfig),
     file: outputPath,
     format: 'esm'
   },
