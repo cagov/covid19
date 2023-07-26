4877df5180a2f413ccb577cf418ae6e47d2a60ff
James Logan <jamesstanleylogan@gmail.com>
2023-07-26 16:45:44 -0700

    support development timestamps

--- pages/_data/stateDashboardCasesUpdateTime.js
+++ pages/_data/stateDashboardCasesUpdateTime.js
@@ -5,7 +5,7 @@
 const fetch = require('node-fetch')
 
 module.exports = function() {
-  let dataDomain = 'https://data.covid19.ca.gov/';
+  let dataDomain = (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'staging') ? 'https://raw.githubusercontent.com/cagov/covid-static-data/CovidStateDashboardTables_Staging/' : 'https://data.covid19.ca.gov/';
     return new Promise((resolve, reject) => {
     fetch(dataDomain+'data/status/last_cases_update.json')
     .then(res => res.json())
