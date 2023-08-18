7ed66eccbe4cd3826772ba14978448c50427c916
James Logan <jamesstanleylogan@gmail.com>
2023-08-18 10:04:00 -0700

    hosp admissions (#7617)
    * hosp admissions
    
    * homepage label sync
    
    * keep hospitalizations svg
    
    * hosp admissions
    
    * hosp admissions
    
    * hosp admissions
    
    * hosp admissions
    
    * hosp admissions
    
    * hosp admissions

--- src/js/main/index.js
+++ src/js/main/index.js
@@ -20,5 +20,6 @@ function getSVG(file, selector) {
     });
   }
   getSVG('sparkline-hospitalizations.svg','.sparkline-hospitalizations');
+  getSVG('sparkline-admissions.svg','.sparkline-admissions');
   getSVG('sparkline-tests.svg','.sparkline-tests');
   getSVG('sparkline-deaths.svg','.sparkline-deaths');
